d4cefd06541aee0af5fa15f04e880ed889a79865
nkatarmal-crest <nirav.katarmal@crestdatasys.com>
2020-04-22 13:48:17 +0530

    Working schema resource

--- main.tf
+++ main.tf
@@ -1,20 +1,22 @@
 provider "mso" {
-    username = "admin"
-    password = "ins3965!ins3965!"
-    url = "https://173.36.219.193"
-    insecure = true
+  username = "admin"
+  password = "Ins3965!Ins3965!"
+  url      = "https://10.23.248.134"
+  insecure = true
 }
 
 resource "mso_schema" "schema1" {
-    schema = "Test1"
-  
+  name          = "nkp"
+  template_name = "temp1"
+  tenant_id     = "5e9fcbc30f00000d06fccecf"
+
 }
 
-resource "mso_schema_site" "schemasite1" {
-    schema = "Test1"
-    template = "Template1"
-    site = "Site1"
-  
-}
+# resource "mso_schema_site" "schemasite1" {
+#     schema = "Test1"
+#     template = "Template1"
+#     site = "Site1"
+
+# }
 
 
