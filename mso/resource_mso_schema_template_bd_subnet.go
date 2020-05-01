cb3db1f6e101dc0c8960cbb0d7f252daaa724fb7
nkatarmal-crest <nirav.katarmal@crestdatasys.com>
2020-05-01 15:55:20 +0530

    Container name changed

--- mso/resource_mso_schema_template_bd_subnet.go
+++ mso/resource_mso_schema_template_bd_subnet.go
@@ -186,11 +186,11 @@ func resourceMSOTemplateBDSubnetRead(d *schema.ResourceData, m interface{}) erro
 							d.Set("scope", models.StripQuotes(subnetsCont.S("scope").String()))
 							d.Set("description", models.StripQuotes(subnetsCont.S("description").String()))
 							d.Set("shared", subnetsCont.S("shared").Data().(bool))
-							if dataCon.Exists("noDefaultGateway") {
-								d.Set("no_default_gateway", dataCon.S("noDefaultGateway").Data().(bool))
+							if subnetsCont.Exists("noDefaultGateway") {
+								d.Set("no_default_gateway", subnetsCont.S("noDefaultGateway").Data().(bool))
 							}
-							if dataCon.Exists("querier") {
-								d.Set("querier", dataCon.S("querier").Data().(bool))
+							if subnetsCont.Exists("querier") {
+								d.Set("querier", subnetsCont.S("querier").Data().(bool))
 							}
 							found = true
 						}
