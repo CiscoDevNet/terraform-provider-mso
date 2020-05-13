61a08a396d9a34ea270b4ec43d23daf17fe2a9ea
Nirav Katarmal <nirav.katarmal@crestdatasys.com>
2020-05-13 16:12:06 +0530

    updated files

--- mso/provider.go
+++ mso/provider.go
@@ -108,7 +108,7 @@ func Provider() terraform.ResourceProvider {
 			"mso_schema_site_anp_epg_subnet":           datasourceMSOSchemaSiteAnpEpgSubnet(),
 			"mso_schema_site_anp_epg_domain":           dataSourceMSOSchemaSiteAnpEpgDomain(),
 			"mso_schema_site_bd_l3out":                 dataSourceMSOSchemaSiteBdL3out(),
-            "mso_schema_site_vrf":                      dataSourceMSOSchemaSiteVrf(),
+                        "mso_schema_site_vrf":                      dataSourceMSOSchemaSiteVrf(),
 		},
 
 		ConfigureFunc: configureClient,
