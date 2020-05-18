34cebdbdafe710e0d14bc083fa124a25639198c3
nkatarmal-crest <nirav.katarmal@crestdatasys.com>
2020-05-18 18:55:30 +0530

    index based operation done

--- mso/resource_mso_schema_template_bd_subnet.go
+++ mso/resource_mso_schema_template_bd_subnet.go
@@ -216,15 +216,6 @@ func resourceMSOTemplateBDSubnetUpdate(d *schema.ResourceData, m interface{}) er
 	log.Printf("[DEBUG] Template BD: Beginning Update")
 	msoClient := m.(*client.Client)
 
-	schemaID := d.Get("schema_id").(string)
-	bdName := d.Get("bd_name").(string)
-	templateName := d.Get("template_name").(string)
-
-	var IP string
-	if ip, ok := d.GetOk("ip"); ok {
-		IP = ip.(string)
-	}
-
 	var Scope string
 	if scope, ok := d.GetOk("scope"); ok {
 		Scope = scope.(string)
@@ -248,14 +239,65 @@ func resourceMSOTemplateBDSubnetUpdate(d *schema.ResourceData, m interface{}) er
 	if d, ok := d.GetOk("description"); ok {
 		Desc = d.(string)
 	}
+	schemaId := d.Get("schema_id").(string)
 
-	path := fmt.Sprintf("/templates/%s/bds/%s/subnets/%s", templateName, bdName, d.Id())
-	bdSubnetStruct := models.NewTemplateBDSubnet("replace", path, IP, Desc, Scope, Shared, NoDefaultGateway, Querier)
-
-	_, err := msoClient.PatchbyID(fmt.Sprintf("api/v1/schemas/%s", schemaID), bdSubnetStruct)
-
+	cont, err := msoClient.GetViaURL(fmt.Sprintf("api/v1/schemas/%s", schemaId))
 	if err != nil {
 		return err
+	}
+
+	count, err := cont.ArrayCount("templates")
+	if err != nil {
+		return fmt.Errorf("No Template found")
+	}
+	stateTemplate := d.Get("template_name").(string)
+	found := false
+	stateBD := d.Get("bd_name")
+	stateIP := d.Get("ip")
+	for i := 0; i < count; i++ {
+		tempCont, err := cont.ArrayElement(i, "templates")
+		if err != nil {
+			return err
+		}
+		apiTemplate := models.StripQuotes(tempCont.S("name").String())
+
+		if apiTemplate == stateTemplate {
+
+			bdCount, err := tempCont.ArrayCount("bds")
+			if err != nil {
+				return fmt.Errorf("Unable to get BD list")
+			}
+			for j := 0; j < bdCount; j++ {
+				bdCont, err := tempCont.ArrayElement(j, "bds")
+				if err != nil {
+					return err
+				}
+				apiBD := models.StripQuotes(bdCont.S("name").String())
+				if apiBD == stateBD {
+
+					count1, err := bdCont.ArrayCount("subnets")
+					if err != nil {
+						return fmt.Errorf("Unable to get Subnet List")
+					}
+					for k := 0; k < count1; k++ {
+						subnetsCont, err := bdCont.ArrayElement(k, "subnets")
+						if err != nil {
+							return fmt.Errorf("Unable to parse the subntes list")
+						}
+						apiIP := models.StripQuotes(subnetsCont.S("ip").String())
+						if apiIP == stateIP {
+							index := k
+							path := fmt.Sprintf("/templates/%s/bds/%s/subnets/%v", apiTemplate, apiBD, index)
+							bdSubnetStruct := models.NewTemplateBDSubnet("replace", path, apiIP, Desc, Scope, Shared, NoDefaultGateway, Querier)
+							_, err := msoClient.PatchbyID(fmt.Sprintf("api/v1/schemas/%s", schemaID), bdSubnetStruct)
+							if err != nil {
+								return err
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 	return resourceMSOTemplateBDSubnetRead(d, m)
 }
@@ -264,15 +306,6 @@ func resourceMSOTemplateBDSubnetDelete(d *schema.ResourceData, m interface{}) er
 	log.Printf("[DEBUG] Template BD: Beginning Update")
 	msoClient := m.(*client.Client)
 
-	schemaID := d.Get("schema_id").(string)
-	bdName := d.Get("bd_name").(string)
-	templateName := d.Get("template_name").(string)
-
-	var IP string
-	if ip, ok := d.GetOk("ip"); ok {
-		IP = ip.(string)
-	}
-
 	var Scope string
 	if scope, ok := d.GetOk("scope"); ok {
 		Scope = scope.(string)
@@ -296,12 +329,65 @@ func resourceMSOTemplateBDSubnetDelete(d *schema.ResourceData, m interface{}) er
 	if d, ok := d.GetOk("description"); ok {
 		Desc = d.(string)
 	}
-	path := fmt.Sprintf("/templates/%s/bds/%s/subnets/%s", templateName, bdName, d.Id())
-	bdSubnetStruct := models.NewTemplateBDSubnet("remove", path, IP, Desc, Scope, Shared, NoDefaultGateway, Querier)
-	_, err := msoClient.PatchbyID(fmt.Sprintf("api/v1/schemas/%s", schemaID), bdSubnetStruct)
+	schemaId := d.Get("schema_id").(string)
 
+	cont, err := msoClient.GetViaURL(fmt.Sprintf("api/v1/schemas/%s", schemaId))
 	if err != nil {
 		return err
+	}
+
+	count, err := cont.ArrayCount("templates")
+	if err != nil {
+		return fmt.Errorf("No Template found")
+	}
+	stateTemplate := d.Get("template_name").(string)
+	found := false
+	stateBD := d.Get("bd_name")
+	stateIP := d.Get("ip")
+	for i := 0; i < count; i++ {
+		tempCont, err := cont.ArrayElement(i, "templates")
+		if err != nil {
+			return err
+		}
+		apiTemplate := models.StripQuotes(tempCont.S("name").String())
+
+		if apiTemplate == stateTemplate {
+
+			bdCount, err := tempCont.ArrayCount("bds")
+			if err != nil {
+				return fmt.Errorf("Unable to get BD list")
+			}
+			for j := 0; j < bdCount; j++ {
+				bdCont, err := tempCont.ArrayElement(j, "bds")
+				if err != nil {
+					return err
+				}
+				apiBD := models.StripQuotes(bdCont.S("name").String())
+				if apiBD == stateBD {
+
+					count1, err := bdCont.ArrayCount("subnets")
+					if err != nil {
+						return fmt.Errorf("Unable to get Subnet List")
+					}
+					for k := 0; k < count1; k++ {
+						subnetsCont, err := bdCont.ArrayElement(k, "subnets")
+						if err != nil {
+							return fmt.Errorf("Unable to parse the subntes list")
+						}
+						apiIP := models.StripQuotes(subnetsCont.S("ip").String())
+						if apiIP == stateIP {
+							index := k
+							path := fmt.Sprintf("/templates/%s/bds/%s/subnets/%v", apiTemplate, apiBD, index)
+							bdSubnetStruct := models.NewTemplateBDSubnet("replace", path, apiIP, Desc, Scope, Shared, NoDefaultGateway, Querier)
+							_, err := msoClient.PatchbyID(fmt.Sprintf("api/v1/schemas/%s", schemaID), bdSubnetStruct)
+							if err != nil {
+								return err
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 	d.SetId("")
 	return nil
