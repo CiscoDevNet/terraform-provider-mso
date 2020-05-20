cddead8696eca35bde5d3db9839f3c8dc1ef8b42
nkatarmal-crest <nirav.katarmal@crestdatasys.com>
2020-05-20 19:14:52 +0530

    removed extra examples

---
+++ examples/schema_site_anp/main.tf
@@ -0,0 +1,13 @@
+provider "mso" {
+  username = "admin"
+  password = "ins3965!ins3965!"
+  url      = "https://173.36.219.193/"
+  insecure = true
+}
+
+resource "mso_schema_site_anp" "anp1" {
+  schema_id     = "5c6c16d7270000c710f8094d"
+  anp_name      = "AP1234"
+  template_name = "Template1"
+  site_id       = "5c7c95d9510000cf01c1ee3d"
+}
