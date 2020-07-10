543f4d2cd2fb3ed23b07a4eb1be3681d9dc5f7e9
nkatarmal-crest <nirav.katarmal@crestdatasys.com>
2020-07-10 14:05:25 +0530

    TPM-1424 Done

--- mso/provider.go
+++ mso/provider.go
@@ -91,10 +91,7 @@ func Provider() terraform.ResourceProvider {
 			"mso_schema_template_anp_epg_useg_attr":     resourceMSOSchemaTemplateAnpEpgUsegAttr(),
 			"mso_schema_site_anp_epg_selector":          resourceMSOSchemaSiteAnpEpgSelector(),
 			"mso_schema_template_vrf_contract":          resourceMSOTemplateVRFContract(),
-<<<<<<< HEAD
-=======
 			"mso_schema_site_external_epg_selector":     resourceMSOSchemaSiteExternalEpgSelector(),
->>>>>>> parent 7271909831210850debf0a53097e8467e7d127de
 		},
 
 		DataSourcesMap: map[string]*schema.Resource{
