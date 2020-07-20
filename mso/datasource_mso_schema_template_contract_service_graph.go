ff3652c38b15f6a9644ece556874c38ee47a78b1
nkatarmal-crest <nirav.katarmal@crestdatasys.com>
2020-07-20 22:02:20 +0530

    TPM-1751 resource, DS added

---
+++ mso/datasource_mso_schema_template_contract_service_graph.go
@@ -0,0 +1,387 @@
+package mso
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/ciscoecosystem/mso-go-client/client"
+	"github.com/ciscoecosystem/mso-go-client/models"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func datasourceTemplateContractServiceGraph() *schema.Resource {
+	return &schema.Resource{
+		Read: datasourceTemplateContractServiceGraphRead,
+
+		Schema: map[string]*schema.Schema{
+			"schema_id": &schema.Schema{
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringLenBetween(1, 1000),
+			},
+
+			"site_id": &schema.Schema{
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringLenBetween(1, 1000),
+			},
+
+			"template_name": &schema.Schema{
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringLenBetween(1, 1000),
+			},
+
+			"contract_name": &schema.Schema{
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringLenBetween(1, 1000),
+			},
+
+			"service_graph_name": &schema.Schema{
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringLenBetween(1, 1000),
+			},
+
+			"node_relationship": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"provider_connector_bd_name": &schema.Schema{
+							Type:         schema.TypeString,
+							Optional:     true,
+							Computed:     true,
+							ValidateFunc: validation.StringLenBetween(1, 1000),
+						},
+
+						"consumer_connector_bd_name": &schema.Schema{
+							Type:         schema.TypeString,
+							Optional:     true,
+							Computed:     true,
+							ValidateFunc: validation.StringLenBetween(1, 1000),
+						},
+
+						"provider_connector_cluster_interface": &schema.Schema{
+							Type:         schema.TypeString,
+							Optional:     true,
+							Computed:     true,
+							ValidateFunc: validation.StringLenBetween(1, 1000),
+						},
+
+						"provider_connector_redirect_policy_tenant": &schema.Schema{
+							Type:         schema.TypeString,
+							Optional:     true,
+							Computed:     true,
+							ValidateFunc: validation.StringLenBetween(1, 1000),
+						},
+
+						"provider_connector_redirect_policy": &schema.Schema{
+							Type:         schema.TypeString,
+							Optional:     true,
+							Computed:     true,
+							ValidateFunc: validation.StringLenBetween(1, 1000),
+						},
+
+						"provider_subnet_ips": &schema.Schema{
+							Type:     schema.TypeList,
+							Optional: true,
+							Computed: true,
+							Elem:     &schema.Schema{Type: schema.TypeString},
+						},
+
+						"consumer_connector_cluster_interface": &schema.Schema{
+							Type:         schema.TypeString,
+							Optional:     true,
+							Computed:     true,
+							ValidateFunc: validation.StringLenBetween(1, 1000),
+						},
+
+						"consumer_connector_redirect_policy_tenant": &schema.Schema{
+							Type:         schema.TypeString,
+							Optional:     true,
+							Computed:     true,
+							ValidateFunc: validation.StringLenBetween(1, 1000),
+						},
+
+						"consumer_connector_redirect_policy": &schema.Schema{
+							Type:         schema.TypeString,
+							Optional:     true,
+							Computed:     true,
+							ValidateFunc: validation.StringLenBetween(1, 1000),
+						},
+
+						"consumer_subnet_ips": &schema.Schema{
+							Type:     schema.TypeList,
+							Optional: true,
+							Computed: true,
+							Elem:     &schema.Schema{Type: schema.TypeString},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func datasourceTemplateContractServiceGraphRead(d *schema.ResourceData, m interface{}) error {
+
+	msoClient := m.(*client.Client)
+	foundTemp := false
+	foundSite := false
+
+	schemaID := d.Get("schema_id").(string)
+	siteID := d.Get("site_id").(string)
+	templateName := d.Get("template_name").(string)
+	contractName := d.Get("contract_name").(string)
+	serviceGraph := d.Get("service_graph_name").(string)
+
+	cont, err := msoClient.GetViaURL(fmt.Sprintf("api/v1/schemas/%s", schemaID))
+	if err != nil {
+		return err
+	}
+	tempCount, err := cont.ArrayCount("templates")
+	if err != nil {
+		return fmt.Errorf("No templates found")
+	}
+
+	temprelationList := make([]interface{}, 0, 1)
+	for i := 0; i < tempCount; i++ {
+		tempCont, err := cont.ArrayElement(i, "templates")
+		if err != nil {
+			return fmt.Errorf("Error in fetch of template")
+		}
+		template := models.StripQuotes(tempCont.S("name").String())
+		if templateName == template {
+			contractCount, err := tempCont.ArrayCount("contracts")
+			if err != nil {
+				return fmt.Errorf("No contracts found")
+			}
+
+			for j := 0; j < contractCount; j++ {
+				contractCont, err := tempCont.ArrayElement(j, "contracts")
+				if err != nil {
+					fmt.Errorf("Error fetching contract")
+				}
+				conName := models.StripQuotes(contractCont.S("name").String())
+				if conName == contractName {
+					if !contractCont.Exists("serviceGraphRelationship") {
+						return fmt.Errorf("No service graph found")
+					} else {
+
+						graphRelation := contractCont.S("serviceGraphRelationship")
+
+						graphRef := models.StripQuotes(graphRelation.S("serviceGraphRef").String())
+						tokens := strings.Split(graphRef, "/")
+						d.Set("service_graph_name", tokens[len(tokens)-1])
+
+						nodeCount, err := graphRelation.ArrayCount("serviceNodesRelationship")
+						if err != nil {
+							return err
+						}
+						for k := 0; k < nodeCount; k++ {
+							relationMap := make(map[string]interface{})
+							node, err := graphRelation.ArrayElement(k, "serviceNodesRelationship")
+							if err != nil {
+								return err
+							}
+							nodeRef := models.StripQuotes(node.S("serviceNodeRef").String())
+							tokensNode := strings.Split(nodeRef, "/")
+							relationMap["node_name"] = tokensNode[len(tokensNode)-1]
+
+							probdRef := models.StripQuotes(node.S("providerConnector", "bdRef").String())
+							probdRefTokens := strings.Split(probdRef, "/")
+							relationMap["provider_connector_bd_name"] = probdRefTokens[len(probdRefTokens)-1]
+
+							conbdRef := models.StripQuotes(node.S("consumerConnector", "bdRef").String())
+							conbdRefTokens := strings.Split(conbdRef, "/")
+							relationMap["consumer_connector_bd_name"] = conbdRefTokens[len(conbdRefTokens)-1]
+
+							temprelationList = append(temprelationList, relationMap)
+						}
+						foundTemp = true
+						break
+					}
+				}
+			}
+		}
+		if foundTemp {
+			break
+		}
+	}
+
+	siterelationList := make([]interface{}, 0, 1)
+	siteCount, err := cont.ArrayCount("sites")
+	if err != nil {
+		return fmt.Errorf("No sites found")
+	}
+	for i := 0; i < siteCount; i++ {
+		siteCont, err := cont.ArrayElement(i, "sites")
+		if err != nil {
+			return fmt.Errorf("Error fetching site")
+		}
+
+		site := models.StripQuotes(siteCont.S("siteId").String())
+		temp := models.StripQuotes(siteCont.S("templateName").String())
+		if siteID == site && temp == templateName {
+			contractCount, err := siteCont.ArrayCount("contracts")
+			if err != nil {
+				return fmt.Errorf("No contracts found in site")
+			}
+
+			for j := 0; j < contractCount; j++ {
+				contractCont, err := siteCont.ArrayElement(j, "contracts")
+				if err != nil {
+					return fmt.Errorf("Error fetching contract from site")
+				}
+
+				conRef := models.StripQuotes(contractCont.S("contractRef").String())
+				conTokens := strings.Split(conRef, "/")
+				conName := conTokens[len(conTokens)-1]
+				if conName == contractName {
+					if !contractCont.Exists("serviceGraphRelationship") {
+						return fmt.Errorf("No service graph found")
+					} else {
+						graphRelation := contractCont.S("serviceGraphRelationship")
+
+						nodeCount, err := graphRelation.ArrayCount("serviceNodesRelationship")
+						if err != nil {
+							return err
+						}
+						for k := 0; k < nodeCount; k++ {
+							relationMap := make(map[string]interface{})
+							node, err := graphRelation.ArrayElement(k, "serviceNodesRelationship")
+							if err != nil {
+								return err
+							}
+
+							relationMap["provider_connector_cluster_interface"] = models.StripQuotes(node.S("providerConnector", "clusterInterface", "dn").String())
+
+							if node.Exists("providerConnector", "redirectPolicy", "dn") {
+								relationMap["provider_connector_redirect_policy"] = models.StripQuotes(node.S("providerConnector", "redirectPolicy", "dn").String())
+							}
+
+							if node.Exists("providerConnector", "subnets") {
+								subCounts, err := node.ArrayCount("providerConnector", "subnets")
+								if err != nil {
+									return err
+								}
+								subList := make([]interface{}, 0, 1)
+								for l := 0; l < subCounts; l++ {
+									subnet, err := node.ArrayElement(l, "providerConnector", "subnets", "ip")
+									if err != nil {
+										return err
+									}
+									subList = append(subList, models.StripQuotes(subnet.String()))
+								}
+								relationMap["provider_subnet_ips"] = subList
+							}
+
+							relationMap["consumer_connector_cluster_interface"] = models.StripQuotes(node.S("consumerConnector", "clusterInterface", "dn").String())
+
+							if node.Exists("consumerConnector", "redirectPolicy", "dn") {
+								relationMap["consumer_connector_redirect_policy"] = models.StripQuotes(node.S("consumerConnector", "redirectPolicy", "dn").String())
+							}
+
+							if node.Exists("consumerConnector", "subnets") {
+								subCounts, err := node.ArrayCount("consumerConnector", "subnets")
+								if err != nil {
+									return err
+								}
+								subList := make([]interface{}, 0, 1)
+								for l := 0; l < subCounts; l++ {
+									subnet, err := node.ArrayElement(l, "consumerConnector", "subnets", "ip")
+									if err != nil {
+										return err
+									}
+									subList = append(subList, models.StripQuotes(subnet.String()))
+								}
+								relationMap["consumer_subnet_ips"] = subList
+							}
+
+							siterelationList = append(siterelationList, relationMap)
+						}
+						foundSite = true
+					}
+				}
+			}
+		}
+		if foundSite {
+			break
+		}
+	}
+
+	if foundSite && foundTemp {
+		length := len(temprelationList)
+		nodeList := make([]interface{}, 0, 1)
+		for i := 0; i < length; i++ {
+			tempMap := temprelationList[i].(map[string]interface{})
+			siteMap := siterelationList[i].(map[string]interface{})
+
+			allMap := make(map[string]interface{})
+			allMap["provider_connector_bd_name"] = tempMap["provider_connector_bd_name"]
+			allMap["consumer_connector_bd_name"] = tempMap["consumer_connector_bd_name"]
+
+			tp := strings.Split(siteMap["provider_connector_cluster_interface"].(string), "/")
+			token := strings.Split(tp[len(tp)-1], "-")
+			allMap["provider_connector_cluster_interface"] = token[1]
+
+			tp = strings.Split(siteMap["consumer_connector_cluster_interface"].(string), "/")
+			token = strings.Split(tp[len(tp)-1], "-")
+			allMap["consumer_connector_cluster_interface"] = token[1]
+
+			if siteMap["provider_connector_redirect_policy"] != nil {
+				tp := strings.Split(siteMap["provider_connector_redirect_policy"].(string), "/")
+				token1 := strings.Split(tp[1], "-")
+				allMap["provider_connector_redirect_policy_tenant"] = token1[1]
+
+				token2 := strings.Split(tp[len(tp)-1], "-")
+				allMap["provider_connector_redirect_policy"] = token2[1]
+			}
+			if siteMap["consumer_connector_redirect_policy"] != nil {
+				tp := strings.Split(siteMap["consumer_connector_redirect_policy"].(string), "/")
+				token1 := strings.Split(tp[1], "-")
+				allMap["consumer_connector_redirect_policy_tenant"] = token1[1]
+
+				token2 := strings.Split(tp[len(tp)-1], "-")
+				allMap["consumer_connector_redirect_policy"] = token2[1]
+			}
+
+			if siteMap["provider_subnet_ips"] != nil {
+				allMap["provider_subnet_ips"] = siteMap["provider_subnet_ips"]
+			}
+			if siteMap["consumer_subnet_ips"] != nil {
+				allMap["consumer_subnet_ips"] = siteMap["consumer_subnet_ips"]
+			}
+
+			nodeList = append(nodeList, allMap)
+		}
+		d.Set("schema_id", schemaID)
+		d.Set("site_id", siteID)
+		d.Set("template_name", templateName)
+		d.Set("node_relationship", nodeList)
+		d.Set("contract_name", contractName)
+
+		if d.Get("service_graph_name") == serviceGraph {
+			d.SetId(serviceGraph)
+		} else {
+			d.SetId("")
+			return fmt.Errorf("No service graph found for given name")
+		}
+	} else {
+		d.SetId("")
+		return fmt.Errorf("No service graph found for given name")
+	}
+
+	log.Printf("[DEBUG] Completed Read Template Contract Service Graph")
+	return nil
+}
---
+++ website/docs/r/schema_template_contract_service_graph.html.markdown
@@ -0,0 +1,60 @@
+---
+layout: "mso"
+page_title: "MSO: mso_schema_template_contract_service_graph"
+sidebar_current: "docs-mso-resource-schema_template_contract_service_graph"
+description: |-
+  Manages MSO Schema Template Contract service graph.
+---
+
+# mso_schema_template_contract_service_graph #
+
+Manages MSO Schema Template Contract service graph.
+
+## Example Usage ##
+
+```hcl
+
+resource "mso_schema_template_contract_service_graph" "one" {
+  schema_id             = "5f11b0e22c00001c4a812a2a"
+  site_id               = "5c7c95b25100008f01c1ee3c"
+  template_name         = "Template1"
+  contract_name         = "UntitledContract1"
+  service_graph_name    = "sg1"
+  node_relationship {
+    provider_connector_bd_name                  = "BD1"
+    consumer_connector_bd_name                  = "BD2"
+    provider_connector_cluster_interface        = "test"
+    consumer_connector_cluster_interface        = "test"
+    provider_connector_redirect_policy_tenant   = "NkAutomation"
+    provider_connector_redirect_policy          = "test2"
+    consumer_connector_redirect_policy_tenant   = "NkAutomation"
+    consumer_connector_redirect_policy          = "test2"
+    provider_subnet_ips = ["1.2.3.4"]
+    consumer_subnet_ips = ["1.2.3.4"]
+  }
+}
+
+```
+
+## Argument Reference ##
+
+* `schema_id` - (Required) SchemaID under which you want to deploy Contract Service Graph.
+* `site_id` - (Required) SiteID under which you want to deploy Contract Service Graph.
+* `template_name` - (Required) Template where Contract Service Graph to be created.
+* `contract_name` - (Required) The name of the contract to manage. There should be an existing contract with this name.
+* `service_graph_name` - (Required) The name of service graph.
+* `node_relationship` - (Required) Service graph node relationship information.
+* `node_relationship.provider_connector_bd_name` - (Required) bd name for provider connector at template level.
+* `node_relationship.consumer_connector_bd_name` - (Required) bd name for consumer connector at template level.
+* `node_relationship.provider_connector_cluster_interface` - (Required) cluster interface for provider connector to attach with node at site level. 
+* `node_relationship.consumer_connector_cluster_interface` - (Required) cluster interface for consumer connector to attach with node at site level.
+* `node_relationship.provider_connector_redirect_policy_tenant` - (Optional) tenant for redirection policy for provider connector at site level. It is required to set redirection policy for provider connector.
+* `node_relationship.provider_connector_redirect_policy` - (Optional) redirection policy for provider connector at site level.
+* `node_relationship.consumer_connector_redirect_policy_tenant` - (Optional) tenant for redirection policy for consumer connector at site level. It is required to set redirection policy for consumer connector.
+* `node_relationship.consumer_connector_redirect_policy` - (Optional) redirection policy for consumer connector at site level.
+
+
+## Attribute Reference ##
+
+No attributes are exported.
+
