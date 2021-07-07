53a43e389006667b4c85532461505957ab7811b3
anvitha jain <anvjain@cisco.com>
2021-07-07 15:17:00 -0700

    Added examples and documentations for multi site associations

--- examples/schema_site/main.tf
+++ examples/schema_site/main.tf
@@ -7,15 +7,62 @@ terraform {
 }
 
 provider "mso" {
-  username = "admin"
-  password = "ins3965!ins3965!"
-  url      = "https://173.36.219.193/"
+  username = "" # <MSO username>
+  password = "" # <MSO pwd>
+  url      = "" # <MSO URL>
   insecure = true
 }
 
-resource "mso_schema_site" "schemasite01" {
-  schema_id      = "5c4d5bb72700000401f80948"
-  site_id        = "5ec240152e00007d763c18b4"
-  template_name  = "Template1"
-  
+resource "mso_site" "site_1" {
+  name             = var.site_name_1
+  username         = "" # <site username>
+  password         = "" # <site pwd>
+  apic_site_id     = "105"
+  urls             = ["https://10.10.100.111"] # <site url>
+  location = {
+    lat  = 78.946
+    long = 95.623
+  }
+}
+
+resource "mso_site" "site_2" {
+  name             = var.site_name_2
+  username         = "" # <site username>
+  password         = "" # <site pwd>
+  apic_site_id     = "106"
+  urls             = ["https://10.10.100.111"] # <site url>
+  location = {
+    lat  = 79.946
+    long = 96.623
+  }
+}
+
+resource "mso_tenant" "tenant_1" {
+  name         = var.tenant_name
+  display_name = var.tenant_name
+  description  = "DemoTenant"
+  site_associations {
+    site_id                = mso_site.site_1.id
+  }
+  site_associations {
+    site_id                = mso_site.site_2.id
+  }
+}
+
+resource "mso_schema" "schema_1" {
+  name          = var.schema_name
+  template_name = var.template_name
+  tenant_id     = mso_tenant.tenant_1.id
+}
+
+resource "mso_schema_site" "schema_site_1" {
+  schema_id      = mso_schema.schema_1.id
+  site_id        = mso_site.ansible_test.id
+  template_name  = var.template_name
+}
+
+resource "mso_schema_site" "schema_site_2" {
+  schema_id      = mso_schema.schema_1.id
+  site_id        = mso_site.site_test_1.id
+  template_name  = var.template_name
 }
