a13a88bf045444e8dbf61afb9fb91b28749daaf3
anvitha jain <anvjain@cisco.com>
2021-09-14 19:46:43 -0700

    Added new resource - Site External EPG

--- examples/schema_site_external_epg/main.tf
+++ examples/schema_site_external_epg/main.tf
@@ -7,31 +7,112 @@ terraform {
 }
 
 provider "mso" {
-  username = "" # <MSO username>
-  password = "" # <MSO pwd>
-  url      = "" # <MSO URL>
+  username = "admin"
+  password = "ins3965!"
+  url      = "https://10.23.248.66/"
   insecure = true
+  platform = "nd"
+}
+
+// resource "mso_site" "test_site" {
+//   name             = "test_site"
+//   username         = "admin" # <APIC username>
+//   password         = "ins3965!" # <APIC pwd>
+//   apic_site_id     = "105"
+//   urls             = ["https://10.23.248.102/"] # <APIC site url>
+//   location = {
+//     lat  = 78.946
+//     long = 95.623
+//   }
+// }
+
+// resource "mso_tenant" "tenant1" {
+//   name          = "test_tenant"
+//   display_name  = "test_tenant"
+//   description   = "DemoTenant"
+//   site_associations {
+//     site_id     = "60a6a86b2156a10d1cf14095"
+//   }
+// }
+
+resource "mso_schema" "schema1" {
+  name          = "test_schema"
+  template_name = "Template1"
+  // tenant_id     = mso_tenant.tenant1.id
+  tenant_id     = "612c7f3e2c0000f863513c87"
+}
+
+
+resource "mso_schema_template_vrf" "vrf1" {
+  schema_id       = mso_schema.schema1.id
+  template        = mso_schema.schema1.template_name
+	name            = "vrf1"
+	display_name    = "vrf1"
+}
+
+resource "mso_schema_template_l3out" "template_l3out" {
+  schema_id             = mso_schema.schema1.id
+  template_name         = mso_schema.schema1.template_name
+  l3out_name            = "l3out1"
+  display_name          = "l3out1"
+  vrf_name              = mso_schema_template_vrf.vrf1.id
+  vrf_schema_id         = mso_schema_template_vrf.vrf1.schema_id
+  vrf_template_name     = mso_schema_template_vrf.vrf1.template
 }
 
 resource "mso_schema_template_external_epg" "template_externalepg" {
-	schema_id           = "5f043b3b2c0000f47e812a0b"
-	template_name       = "Template1"
-	external_epg_name   = "temp_epg"
-    external_epg_type   = "cloud"
-	display_name        = "temp_epg"
-	vrf_name            = "Myvrf"
-    anp_name            = "ap1"
-    l3out_name          = "temp"
-    site_id             = ["5c7c95d9510000cf01c1ee3d"]
-    selector_name       = "check02"
-    selector_ip         = "12.23.34.45"
+	schema_id             = mso_schema.schema1.id
+	template_name         = mso_schema.schema1.template_name
+	external_epg_name     = "temp_epg"
+	display_name          = "temp_epg"
+	vrf_name              = mso_schema_template_vrf.vrf1.id
+  vrf_schema_id         = mso_schema_template_vrf.vrf1.schema_id
+  vrf_template_name     = mso_schema_template_vrf.vrf1.template
 }
 
-resource "mso_schema_site_external_epg_selector" "sel1" {
-  schema_id = "${mso_schema_template_external_epg.template_externalepg.schema_id}"
-  template_name = "${mso_schema_template_external_epg.template_externalepg.template_name}"
-  site_id = "${mso_schema_template_external_epg.template_externalepg.site_id}"
-  external_epg_name = "${mso_schema_template_external_epg.template_externalepg.external_epg_name}"
-  name = "second"
-  ip = "12.25.70.50"
+resource "mso_schema_site" "schema_site_1" {
+  schema_id      = mso_schema.schema1.id
+  site_id        = "60a6a86b2156a10d1cf14095"
+  template_name  = mso_schema.schema1.template_name
+}
+
+resource "mso_schema_site_vrf" "site_vrf" {
+  template_name = mso_schema_site.schema_site_1.template_name
+  site_id       = mso_schema_site.schema_site_1.site_id
+  schema_id     = mso_schema_site.schema_site_1.schema_id
+  vrf_name      = mso_schema_template_vrf.vrf1.name
+}
+
+resource "mso_rest" "site_l3out" {
+  path    = "/mso/api/v1/schemas/${mso_schema_site.schema_site_1.schema_id}?validate=false"
+  method  = "PATCH"
+  payload = <<EOF
+  [
+    {
+      "op": "add",
+      "path": "/sites/${mso_schema_site.schema_site_1.site_id}-${mso_schema_site.schema_site_1.template_name}/intersiteL3outs/-",
+      "value": {
+        "l3outRef": {
+          "l3outName": "${mso_schema_template_l3out.template_l3out.l3out_name}",
+          "schemaId": "${mso_schema_site.schema_site_1.schema_id}",
+          "templateName": "${mso_schema_site.schema_site_1.template_name}"
+        },
+      "vrfRef": {
+          "schemaId": "${mso_schema_site_vrf.site_vrf.schema_id}",
+          "templateName": "${mso_schema_site_vrf.site_vrf.template_name}",
+          "vrfName": "${mso_schema_site_vrf.site_vrf.vrf_name}"
+        }
+      }
+    }
+  ]
+  EOF
+}
+
+resource "mso_schema_site_external_epg" "site_externalepg" {
+  depends_on            = [ mso_rest.site_l3out ]
+  site_id               = mso_schema_site.schema_site_1.site_id
+	schema_id             = mso_schema_site.schema_site_1.schema_id
+	template_name         = mso_schema_template_external_epg.template_externalepg.template_name
+	external_epg_name     = mso_schema_template_external_epg.template_externalepg.external_epg_name
+  l3out_name            = mso_schema_template_l3out.template_l3out.l3out_name
 }
---
+++ mso/datasource_mso_schema_site_external_epg.go
@@ -0,0 +1,118 @@
+package mso
+
+import (
+	"fmt"
+	"log"
+	"regexp"
+
+	"github.com/ciscoecosystem/mso-go-client/client"
+	"github.com/ciscoecosystem/mso-go-client/models"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func dataSourceMSOSchemaSiteExternalEpg() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceMSOSchemaSiteExternalEpgRead,
+
+		SchemaVersion: version,
+
+		Schema: (map[string]*schema.Schema{
+			"schema_id": &schema.Schema{
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringLenBetween(1, 1000),
+			},
+			"template_name": &schema.Schema{
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringLenBetween(1, 1000),
+			},
+			"external_epg_name": &schema.Schema{
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringLenBetween(1, 1000),
+			},
+			"l3out_name": &schema.Schema{
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validation.StringLenBetween(1, 1000),
+			},
+			"site_id": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+		}),
+	}
+}
+
+func resourceMSOSchemaSiteExternalEpgRead(d *schema.ResourceData, m interface{}) error {
+	log.Printf("[DEBUG] %s: Beginning Read", d.Id())
+
+	msoClient := m.(*client.Client)
+
+	schemaId := d.Get("schema_id").(string)
+
+	cont, err := msoClient.GetViaURL(fmt.Sprintf("api/v1/schemas/%s", schemaId))
+	if err != nil {
+		return err
+	}
+	count, err := cont.ArrayCount("sites")
+	if err != nil {
+		return fmt.Errorf("No Sites found")
+	}
+	stateSiteId := d.Get("site_id").(string)
+	found := false
+	stateExternalEpg := d.Get("external_epg_name").(string)
+	for i := 0; i < count; i++ {
+		siteCont, err := cont.ArrayElement(i, "sites")
+		if err != nil {
+			return err
+		}
+		apiSiteId := models.StripQuotes(siteCont.S("siteId").String())
+
+		if apiSiteId == stateSiteId {
+			externalEpgCount, err := siteCont.ArrayCount("externalEpgs")
+			if err != nil {
+				return fmt.Errorf("Unable to get External EPG list")
+			}
+			for j := 0; j < externalEpgCount; j++ {
+				externalEpgCont, err := siteCont.ArrayElement(j, "externalEpgs")
+				if err != nil {
+					return err
+				}
+				externalEpgRef := models.StripQuotes(externalEpgCont.S("externalEpgRef").String())
+				re := regexp.MustCompile("/schemas/(.*)/templates/(.*)/externalEpgs/(.*)")
+				match := re.FindStringSubmatch(externalEpgRef)
+				if match[3] == stateExternalEpg {
+					d.SetId(match[3])
+					d.Set("external_epg_name", match[3])
+					d.Set("schema_id", match[1])
+					d.Set("template_name", match[2])
+					d.Set("site_id", apiSiteId)
+
+					l3outRef := models.StripQuotes(externalEpgCont.S("l3outRef").String())
+					reL3out := regexp.MustCompile("/schemas/(.*)/templates/(.*)/l3outs/(.*)")
+					matchL3out := reL3out.FindStringSubmatch(l3outRef)
+					d.Set("l3out_name", matchL3out[3])
+
+					found = true
+					break
+				}
+			}
+		}
+	}
+
+	if !found {
+		d.SetId("")
+	}
+
+	log.Printf("[DEBUG] %s: Read finished successfully", d.Id())
+	return nil
+
+}
