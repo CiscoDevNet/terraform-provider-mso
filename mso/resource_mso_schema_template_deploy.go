b707f65f3c1b4ace40cba969b625d96addeba2d0
Lionel Hercot <lhercot@cisco.com>
2021-12-16 03:04:25 +0100

    [BEHAVIOR CHANGE] Fix mso_schema_template_deploy so destroy undeploy all sites

--- mso/resource_mso_schema_template_deploy.go
+++ mso/resource_mso_schema_template_deploy.go
@@ -2,8 +2,10 @@ package mso
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/ciscoecosystem/mso-go-client/client"
+	"github.com/ciscoecosystem/mso-go-client/models"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 )
@@ -12,6 +14,7 @@ func resourceMSOSchemaTemplateDeploy() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceMSOSchemaTemplateDeployCreate,
 		Read:   resourceMSOSchemaTemplateDeployRead,
+		Update: resourceMSOSchemaTemplateDeployCreate,
 		Delete: resourceMSOSchemaTemplateDeployDelete,
 
 		SchemaVersion: version,
@@ -31,18 +34,22 @@ func resourceMSOSchemaTemplateDeploy() *schema.Resource {
 				ValidateFunc: validation.StringLenBetween(1, 1000),
 			},
 
+			"force_apply": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "re-deploy",
+			},
+
 			"undeploy": &schema.Schema{
 				Type:     schema.TypeBool,
 				Optional: true,
 				Computed: true,
-				ForceNew: true,
 			},
 
 			"site_id": &schema.Schema{
 				Type:         schema.TypeString,
 				Optional:     true,
 				Computed:     true,
-				ForceNew:     true,
 				ValidateFunc: validation.StringLenBetween(1, 1000),
 			},
 		}),
@@ -50,6 +57,7 @@ func resourceMSOSchemaTemplateDeploy() *schema.Resource {
 }
 
 func resourceMSOSchemaTemplateDeployCreate(d *schema.ResourceData, m interface{}) error {
+	log.Printf("[DEBUG] %s: Beginning Template Deploy", d.Id())
 	templateName := d.Get("template_name").(string)
 	schemaID := d.Get("schema_id").(string)
 
@@ -76,16 +84,52 @@ func resourceMSOSchemaTemplateDeployCreate(d *schema.ResourceData, m interface{}
 		return err
 	}
 	d.SetId(schemaID)
+	log.Printf("[DEBUG] %s: Template deployed successfully", d.Id())
 	return resourceMSOSchemaTemplateDeployRead(d, m)
 }
 
 func resourceMSOSchemaTemplateDeployRead(d *schema.ResourceData, m interface{}) error {
 	// We set this intentionally blank so that we execute this in every run.
-	d.Set("template_name", "")
+	d.Set("force_apply", "")
 	return nil
 }
 
 func resourceMSOSchemaTemplateDeployDelete(d *schema.ResourceData, m interface{}) error {
+	log.Printf("[DEBUG] %s: Beginning Template Undeploy", d.Id())
+	msoClient := m.(*client.Client)
+	templateName := d.Get("template_name").(string)
+	schemaID := d.Get("schema_id").(string)
+	schemaCont, err := msoClient.GetViaURL(fmt.Sprintf("api/v1/schemas/%s", schemaID))
+	if err != nil {
+		return err
+	}
+
+	siteCount, err := schemaCont.ArrayCount("sites")
+	if err != nil {
+		return err
+	}
+
+	for i := 0; i < siteCount; i++ {
+		siteCont, err := schemaCont.ArrayElement(i, "sites")
+		if err != nil {
+			return err
+		}
+
+		currentSiteId := models.StripQuotes(siteCont.S("siteId").String())
+		currentTemplateName := models.StripQuotes(siteCont.S("templateName").String())
+
+		if currentTemplateName == templateName {
+			log.Printf("[DEBUG] %s: Undeploying site: %s for Template: %s", d.Id(), currentSiteId, currentTemplateName)
+			queryString := fmt.Sprintf("?undeploy=%s", currentSiteId)
+			path := fmt.Sprintf("/api/v1/execute/schema/%s/template/%s%s", schemaID, templateName, queryString)
+			_, err := msoClient.GetViaURL(path)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
 	d.SetId("")
+	log.Printf("[DEBUG] %s: Template undeployed successfully", d.Id())
 	return nil
 }
