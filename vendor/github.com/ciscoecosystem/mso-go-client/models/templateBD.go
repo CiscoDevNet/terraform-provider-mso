3a9166c77feadc8f1c7a26f99ff44c1873eb2fbd
Shreyas <ssrish@cisco.com>
2022-02-09 10:33:45 -0800

    add vendor files

--- vendor/github.com/ciscoecosystem/mso-go-client/models/templateBD.go
+++ vendor/github.com/ciscoecosystem/mso-go-client/models/templateBD.go
@@ -6,12 +6,18 @@ type TemplateBD struct {
 	Value map[string]interface{} `json:",omitempty"`
 }
 
-func NewTemplateBD(ops, path, name, displayName, layer2Unicast string, intersiteBumTrafficAllow, optimizeWanBandwidth, l2Stretch, l3MCast bool, vrfRef, dhcpLabel map[string]interface{}) *TemplateBD {
+func NewTemplateBD(ops, path, name, displayName, layer2Unicast, unkMcastAct, multiDstPktAct, v6unkMcastAct, vmac string, intersiteBumTrafficAllow, optimizeWanBandwidth, l2Stretch, l3MCast, arpFlood, unicastRouting bool, vrfRef, dhcpLabel map[string]interface{}) *TemplateBD {
 	var bdMap map[string]interface{}
 	bdMap = map[string]interface{}{
 		"name":                     name,
 		"displayName":              displayName,
 		"l2UnknownUnicast":         layer2Unicast,
+		"unkMcastAct":              unkMcastAct,
+		"multiDstPktAct":           multiDstPktAct,
+		"v6unkMcastAct":            v6unkMcastAct,
+		"vmac":                     vmac,
+		"arpFlood":                 arpFlood,
+		"unicastRouting":           unicastRouting,
 		"intersiteBumTrafficAllow": intersiteBumTrafficAllow,
 		"optimizeWanBandwidth":     optimizeWanBandwidth,
 		"l2Stretch":                l2Stretch,
@@ -25,7 +31,27 @@ func NewTemplateBD(ops, path, name, displayName, layer2Unicast string, intersite
 		bdMap["l2UnknownUnicast"] = "flood"
 	}
 
-	if bdMap["dhcpLabel"] == nil {
+	if bdMap["unkMcastAct"] == "optimized_flooding" {
+		bdMap["unkMcastAct"] = "opt-flood"
+	} else {
+		bdMap["unkMcastAct"] = "flood"
+	}
+
+	if bdMap["multiDstPktAct"] == "flood_in_bd" || bdMap["multiDstPktAct"] == "" {
+		bdMap["multiDstPktAct"] = "bd-flood"
+	}
+
+	if bdMap["multiDstPktAct"] == "flood_in_encap" {
+		bdMap["multiDstPktAct"] = "encap-flood"
+	}
+
+	if bdMap["v6unkMcastAct"] == "optimized_flooding" {
+		bdMap["v6unkMcastAct"] = "opt-flood"
+	} else {
+		bdMap["v6unkMcastAct"] = "flood"
+	}
+
+	if len(dhcpLabel) == 0 {
 		delete(bdMap, "dhcpLabel")
 	}
 
