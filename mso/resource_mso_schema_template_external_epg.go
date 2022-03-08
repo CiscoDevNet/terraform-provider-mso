c671fd37c5497ec9bb369dfbc843e43ea5349fc2
hthakkar-crest <harsh.thakkar@crestdatasys.com>
2022-03-08 16:49:05 +0530

    updated schema_template_external_epg

--- mso/resource_mso_schema_template_external_epg.go
+++ mso/resource_mso_schema_template_external_epg.go
@@ -219,12 +219,13 @@ func resourceMSOTemplateExtenalepgImport(d *schema.ResourceData, m interface{}) 
 					epgType := d.Get("external_epg_type").(string)
 					if epgType == "cloud" {
 						selList := externalepgCont.S("selectors").Data().([]interface{})
-
-						selector := selList[0].(map[string]interface{})
-						d.Set("selector_name", selector["name"])
-						expList := selector["expressions"].([]interface{})
-						exp := expList[0].(map[string]interface{})
-						d.Set("selector_ip", exp["value"])
+						if len(selList) > 0 {
+							selector := selList[0].(map[string]interface{})
+							d.Set("selector_name", selector["name"])
+							expList := selector["expressions"].([]interface{})
+							exp := expList[0].(map[string]interface{})
+							d.Set("selector_ip", exp["value"])
+						}
 					} else {
 						d.Set("site_id", make([]interface{}, 0, 1))
 						d.Set("selector_name", "")
