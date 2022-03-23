2734f405b18637fe73afda268b33bd97b716f98c
cindy zhao <xinyuezhao96@gmail.com>
2022-03-23 00:15:41 -0700

    [bugfix] update example for schema_template_external_epg_contract

--- examples/schema_template_external_epg_contract/main.tf
+++ examples/schema_template_external_epg_contract/main.tf
@@ -13,12 +13,86 @@ provider "mso" {
   insecure = true
 }
 
-resource "mso_schema_template_external_epg_contract" "c1" {
-  schema_id                 = "5ea809672c00003bc40a2799"
-  template_name             = "Template1"
-  contract_name             = "contractdemo"
-  external_epg_name         = "UntitledExternalEPG1"
+resource "mso_tenant" "test_tenant" {
+  name         = "eepg_contract_tenant"
+  display_name = "eepg_contract_tenant"
+  description  = "DemoTenant"
+}
+
+resource "mso_schema" "test_schema" {
+  name          = "eepg_contract_schema"
+  template_name = "eepg_contract_template"
+  tenant_id     = mso_tenant.test_tenant.id
+}
+
+resource "mso_schema_template_vrf" "test_vrf" {
+  schema_id        = mso_schema.test_schema.id
+  template         = mso_schema.test_schema.template_name
+  name             = "eepg_contract_vrf"
+  display_name     = "eepg_contract_vrf"
+  layer3_multicast = false
+}
+
+resource "mso_schema_template_external_epg" "template_externalepg" {
+  schema_id         = mso_schema.test_schema.id
+  template_name     = mso_schema.test_schema.template_name
+  external_epg_name = "eepg"
+  display_name      = "eepg"
+  vrf_name          = mso_schema_template_vrf.test_vrf.name
+  external_epg_type = "on-premise"
+}
+
+resource "mso_schema_template_filter_entry" "filter_entry" {
+  schema_id            = mso_schema.test_schema.id
+  template_name        = mso_schema.test_schema.template_name
+  name                 = "Filter1"
+  display_name         = "Filter1"
+  entry_name           = "entry1"
+  entry_display_name   = "entry1"
+  entry_description    = "DemoEntry"
+  ether_type           = "arp"
+  ip_protocol          = "eigrp"
+  tcp_session_rules    = ["acknowledgement"]
+  destination_from     = "unspecified"
+  destination_to       = "unspecified"
+  source_from          = "unspecified"
+  source_to            = "unspecified"
+  arp_flag             = "unspecified"
+  stateful             = false
+  match_only_fragments = false
+}
+
+resource "mso_schema_template_contract" "template_contract" {
+  schema_id     = mso_schema.test_schema.id
+  template_name = mso_schema.test_schema.template_name
+  contract_name = "Contract1"
+  display_name  = "Contract1"
+  filter_type   = "bothWay"
+  scope         = "context"
+  filter_relationship {
+    filter_schema_id     = mso_schema_template_filter_entry.filter_entry.schema_id
+    filter_template_name = mso_schema_template_filter_entry.filter_entry.template_name
+    filter_name          = mso_schema_template_filter_entry.filter_entry.name
+  }
+  directives = ["none"]
+}
+
+resource "mso_schema_template_external_epg_contract" "consumer_contract" {
+  schema_id                 = mso_schema.test_schema.id
+  template_name             = mso_schema.test_schema.template_name
+  contract_name             = mso_schema_template_contract.template_contract.contract_name
+  external_epg_name         = mso_schema_template_external_epg.template_externalepg.external_epg_name
   relationship_type         = "consumer"
-  contract_schema_id        = "5ea809672c00003bc40a2799"
-  contract_template_name    = "Template1"
+  contract_schema_id        = mso_schema_template_contract.template_contract.schema_id
+  contract_template_name    = mso_schema_template_contract.template_contract.template_name
+}
+
+resource "mso_schema_template_external_epg_contract" "provider_contract" {
+  schema_id                 = mso_schema.test_schema.id
+  template_name             = mso_schema.test_schema.template_name
+  contract_name             = mso_schema_template_contract.template_contract.contract_name
+  external_epg_name         = mso_schema_template_external_epg.template_externalepg.external_epg_name
+  relationship_type         = "provider"
+  contract_schema_id        = mso_schema_template_contract.template_contract.schema_id
+  contract_template_name    = mso_schema_template_contract.template_contract.template_name
 }
