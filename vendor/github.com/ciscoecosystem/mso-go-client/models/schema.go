04e38dbaf5d919b8c5554d798e20e70e62a4568d
Shreyas <ssrish@cisco.com>
2022-04-13 09:31:44 -0700

    [ignore] update vendor files

--- vendor/github.com/ciscoecosystem/mso-go-client/models/schema.go
+++ vendor/github.com/ciscoecosystem/mso-go-client/models/schema.go
@@ -9,27 +9,61 @@ type Schema struct {
 	Sites []map[string]interface{} `json:",omitempty"`
 }
 
-func NewSchema(id, displayName, templateName, tenantId string) *Schema {
-	templateMap := map[string]interface{}{
-		"name":          templateName,
-		"tenantId":      tenantId,
-		"displayName":   templateName,
-		"anps":          []interface{}{},
-		"contracts":     []interface{}{},
-		"vrfs":          []interface{}{},
-		"bds":           []interface{}{},
-		"filters":       []interface{}{},
-		"externalEpgs":  []interface{}{},
-		"serviceGraphs": []interface{}{},
+type SchemaReplace struct {
+	Ops   string                   `json:",omitempty"`
+	Path  string                   `json:",omitempty"`
+	Value []map[string]interface{} `json:",omitempty"`
+}
+
+func NewSchema(id, displayName, templateName, tenantId string, template []interface{}) (*Schema, *SchemaReplace) {
+	result := []map[string]interface{}{}
+	if templateName != "" {
+		templateMap := map[string]interface{}{
+			"name":          templateName,
+			"tenantId":      tenantId,
+			"displayName":   templateName,
+			"anps":          []interface{}{},
+			"contracts":     []interface{}{},
+			"vrfs":          []interface{}{},
+			"bds":           []interface{}{},
+			"filters":       []interface{}{},
+			"externalEpgs":  []interface{}{},
+			"serviceGraphs": []interface{}{},
+		}
+		result = []map[string]interface{}{
+			templateMap,
+		}
+	} else {
+		for _, map_values := range template {
+			map_template_values := map_values.(map[string]interface{})
+			templateMap := map[string]interface{}{
+				"name":          map_template_values["name"],
+				"tenantId":      map_template_values["tenantId"],
+				"displayName":   map_template_values["displayName"],
+				"anps":          []interface{}{},
+				"contracts":     []interface{}{},
+				"vrfs":          []interface{}{},
+				"bds":           []interface{}{},
+				"filters":       []interface{}{},
+				"externalEpgs":  []interface{}{},
+				"serviceGraphs": []interface{}{},
+			}
+			result = append(result, templateMap)
+		}
 	}
-	templates := []map[string]interface{}{
-		templateMap,
-	}
-	return &Schema{
-		Id:          id,
-		DisplayName: displayName,
-		Templates:   templates,
-		Sites:       []map[string]interface{}{},
+	if id == "" {
+		return &Schema{
+			Id:          id,
+			DisplayName: displayName,
+			Templates:   result,
+			Sites:       []map[string]interface{}{},
+		}, nil
+	} else {
+		return nil, &SchemaReplace{
+			Ops:   "replace",
+			Path:  "/templates",
+			Value: result,
+		}
 	}
 }
 
@@ -41,4 +75,15 @@ func (schema *Schema) ToMap() (map[string]interface{}, error) {
 	A(schemaAttributeMap, "sites", schema.Sites)
 
 	return schemaAttributeMap, nil
+}
+
+func (schemaReplace *SchemaReplace) ToMap() (map[string]interface{}, error) {
+	schemaReplaceMap := make(map[string]interface{})
+	A(schemaReplaceMap, "op", schemaReplace.Ops)
+	A(schemaReplaceMap, "path", schemaReplace.Path)
+	if schemaReplace.Value != nil {
+		A(schemaReplaceMap, "value", schemaReplace.Value)
+	}
+
+	return schemaReplaceMap, nil
 }
