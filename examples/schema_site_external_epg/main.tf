aad2f344bcc37fa903e2397cee1ed3180260bf84
akinross <akinross@cisco.com>
2023-05-11 10:53:37 +0200

    [minor_change] update examples for site external epg

--- examples/schema_site_external_epg/main.tf
+++ examples/schema_site_external_epg/main.tf
@@ -14,103 +14,63 @@ provider "mso" {
   platform = "nd"
 }
 
-resource "mso_site" "test_site" {
-  name         = "test_site"
-  username     = "" # <APIC username>
-  password     = "" # <APIC pwd>
-  apic_site_id = "105"
-  urls         = "" # <APIC site url>
-  location = {
-    lat  = 78.946
-    long = 95.623
+data "mso_tenant" "demo_tenant" {
+  name         = "demo_tenant"
+  display_name = "demo_tenant"
+}
+
+data "mso_site" "demo_site" {
+  name = "demo_site"
+}
+
+resource "mso_schema" "schema_blocks" {
+  name = "demo_schema_blocks"
+  template {
+    name         = "Template1"
+    display_name = "Template1"
+    tenant_id    = data.mso_tenant.demo_tenant.id
   }
 }
 
-resource "mso_tenant" "tenant1" {
-  name         = "test_tenant"
-  display_name = "test_tenant"
-  description  = "DemoTenant"
-  site_associations {
-    site_id = mso_site.test_site.id
-  }
-}
-
-resource "mso_schema" "schema1" {
-  name          = "test_schema"
-  template_name = "Template1"
-  tenant_id     = mso_tenant.tenant1.id
+resource "mso_schema_site" "demo_schema_site" {
+  schema_id     = mso_schema.schema_blocks.id
+  template_name = one(mso_schema.schema_blocks.template).name
+  site_id       = data.mso_site.demo_site.id
 }
 
 resource "mso_schema_template_vrf" "vrf1" {
-  schema_id    = mso_schema.schema1.id
-  template     = mso_schema.schema1.template_name
-  name         = "vrf1"
-  display_name = "vrf1"
+  schema_id              = mso_schema.schema_blocks.id
+  template               = one(mso_schema.schema_blocks.template).name
+  name                   = "vrf1"
+  display_name           = "vrf1"
+  ip_data_plane_learning = "enabled"
 }
 
 resource "mso_schema_template_l3out" "template_l3out" {
-  schema_id         = mso_schema.schema1.id
-  template_name     = mso_schema.schema1.template_name
+  schema_id         = mso_schema.schema_blocks.id
+  template_name     = one(mso_schema.schema_blocks.template).name
   l3out_name        = "l3out1"
   display_name      = "l3out1"
-  vrf_name          = mso_schema_template_vrf.vrf1.id
+  vrf_name          = mso_schema_template_vrf.vrf1.name
   vrf_schema_id     = mso_schema_template_vrf.vrf1.schema_id
   vrf_template_name = mso_schema_template_vrf.vrf1.template
 }
 
-resource "mso_schema_template_external_epg" "template_externalepg" {
-  schema_id         = mso_schema.schema1.id
-  template_name     = mso_schema.schema1.template_name
-  external_epg_name = "temp_epg"
-  display_name      = "temp_epg"
-  vrf_name          = mso_schema_template_vrf.vrf1.id
+resource "mso_schema_template_external_epg" "extepg1" {
+  schema_id         = mso_schema.schema_blocks.id
+  template_name     = one(mso_schema.schema_blocks.template).name
+  external_epg_name = "extepg1"
+  display_name      = "extepg1"
+  vrf_name          = mso_schema_template_vrf.vrf1.name
   vrf_schema_id     = mso_schema_template_vrf.vrf1.schema_id
   vrf_template_name = mso_schema_template_vrf.vrf1.template
+  l3out_name        = mso_schema_template_l3out.template_l3out.l3out_name
 }
 
-resource "mso_schema_site" "schema_site_1" {
-  schema_id     = mso_schema.schema1.id
-  site_id       = mso_site.test_site.id
-  template_name = mso_schema.schema1.template_name
-}
-
-resource "mso_schema_site_vrf" "site_vrf" {
-  template_name = mso_schema_site.schema_site_1.template_name
-  site_id       = mso_schema_site.schema_site_1.site_id
-  schema_id     = mso_schema_site.schema_site_1.schema_id
-  vrf_name      = mso_schema_template_vrf.vrf1.name
-}
-
-resource "mso_rest" "site_l3out" {
-  path    = "/mso/api/v1/schemas/${mso_schema_site.schema_site_1.schema_id}?validate=false"
-  method  = "PATCH"
-  payload = <<EOF
-  [
-    {
-      "op": "add",
-      "path": "/sites/${mso_schema_site.schema_site_1.site_id}-${mso_schema_site.schema_site_1.template_name}/intersiteL3outs/-",
-      "value": {
-        "l3outRef": {
-          "l3outName": "${mso_schema_template_l3out.template_l3out.l3out_name}",
-          "schemaId": "${mso_schema_site.schema_site_1.schema_id}",
-          "templateName": "${mso_schema_site.schema_site_1.template_name}"
-        },
-      "vrfRef": {
-          "schemaId": "${mso_schema_site_vrf.site_vrf.schema_id}",
-          "templateName": "${mso_schema_site_vrf.site_vrf.template_name}",
-          "vrfName": "${mso_schema_site_vrf.site_vrf.vrf_name}"
-        }
-      }
-    }
-  ]
-  EOF
-}
-
-resource "mso_schema_site_external_epg" "site_externalepg" {
-  depends_on        = [mso_rest.site_l3out]
-  site_id           = mso_schema_site.schema_site_1.site_id
-  schema_id         = mso_schema_site.schema_site_1.schema_id
-  template_name     = mso_schema_template_external_epg.template_externalepg.template_name
-  external_epg_name = mso_schema_template_external_epg.template_externalepg.external_epg_name
+resource "mso_schema_site_external_epg" "site_extepg1" {
+  site_id           = mso_schema_site.demo_schema_site.id
+  schema_id         = mso_schema.schema_blocks.id
+  template_name     = one(mso_schema.schema_blocks.template).name
+  external_epg_name = mso_schema_template_external_epg.extepg1.external_epg_name
   l3out_name        = mso_schema_template_l3out.template_l3out.l3out_name
 }
