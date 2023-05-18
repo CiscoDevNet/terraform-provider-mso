bad6c5357330c389af67612078fa59240ce7511f
Sabari Jaganathan <93724860+sajagana@users.noreply.github.com>
2023-05-18 18:38:19 +0530

    [ignore] added errorForObjectNotFound function to the mso util.go

--- mso/util.go
+++ mso/util.go
@@ -1,5 +1,13 @@
 package mso
 
+import (
+	"log"
+	"strings"
+
+	"github.com/ciscoecosystem/mso-go-client/container"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
 const version = 1
 
 func toStringList(configured interface{}) []string {
@@ -9,6 +17,19 @@ func toStringList(configured interface{}) []string {
 		vs = append(vs, val)
 	}
 	return vs
+}
+
+func errorForObjectNotFound(err error, dn string, con *container.Container, d *schema.ResourceData) error {
+	if err != nil {
+		if con.S("code").String() == "404" || strings.HasSuffix(err.Error(), "not found") {
+			log.Printf("[WARN] %s, removing from state: %s", err, dn)
+			d.SetId("")
+			return nil
+		} else {
+			return err
+		}
+	}
+	return nil
 }
 
 // CHANGE TO UTILS IF YOU ADD MORE FUNCTIONS
