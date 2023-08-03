9e9008d8f73f14b509dc3ffaec3cc947dccf9163
akinross <akinross@cisco.com>
2023-08-03 13:51:25 +0200

    [bugfix] fix schema attributes and documentation to reflect computed (Read-Only) attributes correctly and add template_name check for datasource_mso_schema_site

--- mso/datasource_mso_schema_site.go
+++ mso/datasource_mso_schema_site.go
@@ -5,7 +5,6 @@ import (
 	"log"
 
 	"github.com/ciscoecosystem/mso-go-client/client"
-	"github.com/ciscoecosystem/mso-go-client/models"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 )
@@ -28,17 +27,14 @@ func datasourceMSOSchemaSite() *schema.Resource {
 				Required:     true,
 				ValidateFunc: validation.StringLenBetween(1, 1000),
 			},
-
 			"template_name": &schema.Schema{
 				Type:         schema.TypeString,
-				Optional:     true,
+				Required:     true,
 				ValidateFunc: validation.StringLenBetween(1, 1000),
 			},
-
 			"site_id": &schema.Schema{
-				Type:         schema.TypeString,
-				Optional:     true,
-				ValidateFunc: validation.StringLenBetween(1, 1000),
+				Type:     schema.TypeString,
+				Computed: true,
 			},
 		}),
 	}
@@ -50,65 +46,34 @@ func datasourceMSOSchemaSiteRead(d *schema.ResourceData, m interface{}) error {
 	msoClient := m.(*client.Client)
 	name := d.Get("name").(string)
 	schemaId := d.Get("schema_id").(string)
+	templateName := d.Get("template_name").(string)
 	con, err := msoClient.GetViaURL(fmt.Sprintf("api/v1/sites"))
 	if err != nil {
 		return err
 	}
 
-	data := con.S("sites").Data().([]interface{})
 	var flag bool
-	var count int
-	for _, info := range data {
+	var siteId string
+	for _, info := range con.S("sites").Data().([]interface{}) {
 		val := info.(map[string]interface{})
 		if val["name"].(string) == name {
 			flag = true
+			siteId = val["id"].(string)
 			break
 		}
-		count = count + 1
 	}
 	if flag != true {
 		return fmt.Errorf("Site of specified name not found")
 	}
-
-	dataCon := con.S("sites").Index(count)
-	stateSiteId := models.StripQuotes(dataCon.S("id").String())
-
-	cont, err := msoClient.GetViaURL(fmt.Sprintf("api/v1/schemas/%s", schemaId))
+	_, err = getSiteFromSiteIdAndTemplate(schemaId, siteId, templateName, msoClient)
 	if err != nil {
 		return err
 	}
 
-	countSites, err := cont.ArrayCount("sites")
-	if err != nil {
-		return fmt.Errorf("No Template found")
-	}
-
-	found := false
-
-	for i := 0; i < countSites; i++ {
-		tempCont, err := cont.ArrayElement(i, "sites")
-		if err != nil {
-			return err
-		}
-		apiSiteId := models.StripQuotes(tempCont.S("siteId").String())
-		apiTemplate := models.StripQuotes(tempCont.S("templateName").String())
-
-		if apiSiteId == stateSiteId {
-			d.SetId(apiSiteId)
-			d.Set("schema_id", schemaId)
-			d.Set("site_id", apiSiteId)
-			d.Set("template_name", apiTemplate)
-			found = true
-		}
-
-	}
-
-	if !found {
-		d.SetId("")
-		d.Set("schema_id", "")
-		d.Set("site_id", "")
-		d.Set("template_name", "")
-	}
+	d.SetId(fmt.Sprintf("%s/sites/%s-%s", schemaId, siteId, templateName))
+	d.Set("schema_id", schemaId)
+	d.Set("site_id", siteId)
+	d.Set("template_name", templateName)
 
 	log.Printf("[DEBUG] %s: Read finished successfully", d.Id())
 	return nil
--- website/docs/d/schema_site.html.markdown
+++ website/docs/d/schema_site.html.markdown
@@ -3,30 +3,31 @@ layout: "mso"
 page_title: "MSO: mso_schema_site"
 sidebar_current: "docs-mso-data-source-schema_site"
 description: |-
-  Data source for MSO Schema Site
+  Data source for MSO Schema Site.
 ---
 
 # mso_schema_site #
 
-Data source for MSO schema site, to fetch the MSO schema site details.
+Data source for MSO Schema Site.
 
 ## Example Usage ##
 
 ```hcl
 
-data "mso_schema_site" "schema_site" {
-  name       = "sitename"
-  schema_id  = data.mso_schema.schema1.id
+data "mso_schema_site" "example" {
+  name          = "sitename"
+  schema_id     = data.mso_schema.schema1.id
+  template_name = "Template1"
 }
 
 ```
 
 ## Argument Reference ##
 
-* `name` - (Required) name of the site to fetch.
-* `schema_id` - (Required) The schema-id where site is associated.
+* `name` - (Required) The name of the Site.
+* `schema_id` - (Required) The schema ID where the Site is associated.
+* `template_name` - (Required) The name of the template attached to the Site.
 
 ## Attribute Reference ##
 
-* `template_name` - (Optional) The name of the template deployed to the site.
-* `site_id` - (Optional) Site id is set to the MSO site UUID.
+* `site_id` - (Read-Only) The ID (UUID) of the Site.
