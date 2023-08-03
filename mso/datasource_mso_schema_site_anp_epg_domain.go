81a6f2ed0da208b150a59836bf4e7673eed615ce
akinross <akinross@cisco.com>
2023-08-03 17:40:38 +0200

    [bugfix] fix schema attributes and documentation to reflect computed (Read-Only) attributes correctly and add template_name check and for datasource_mso_schema_site_anp_epg_domain and change the id setting for datasource

--- mso/datasource_mso_schema_site_anp_epg_domain.go
+++ mso/datasource_mso_schema_site_anp_epg_domain.go
@@ -3,7 +3,6 @@ package mso
 import (
 	"fmt"
 	"log"
-	"strings"
 
 	"github.com/ciscoecosystem/mso-go-client/client"
 	"github.com/ciscoecosystem/mso-go-client/models"
@@ -21,7 +20,6 @@ func dataSourceMSOSchemaSiteAnpEpgDomain() *schema.Resource {
 			"schema_id": &schema.Schema{
 				Type:         schema.TypeString,
 				Required:     true,
-				ForceNew:     true,
 				ValidateFunc: validation.StringLenBetween(1, 1000),
 			},
 			"template_name": &schema.Schema{
@@ -32,32 +30,28 @@ func dataSourceMSOSchemaSiteAnpEpgDomain() *schema.Resource {
 			"site_id": &schema.Schema{
 				Type:         schema.TypeString,
 				Required:     true,
-				ForceNew:     true,
 				ValidateFunc: validation.StringLenBetween(1, 1000),
 			},
 			"anp_name": &schema.Schema{
 				Type:         schema.TypeString,
 				Required:     true,
-				ForceNew:     true,
 				ValidateFunc: validation.StringLenBetween(1, 1000),
 			},
 			"epg_name": &schema.Schema{
 				Type:         schema.TypeString,
 				Required:     true,
-				ForceNew:     true,
 				ValidateFunc: validation.StringLenBetween(1, 1000),
 			},
-
 			"domain_name": &schema.Schema{
 				Type:         schema.TypeString,
 				Optional:     true,
-				ForceNew:     true,
+				Computed:     true,
 				ValidateFunc: validation.StringLenBetween(1, 1000),
 			},
 			"domain_type": &schema.Schema{
 				Type:     schema.TypeString,
 				Optional: true,
-				ForceNew: true,
+				Computed: true,
 				ValidateFunc: validation.StringInSlice([]string{
 					"vmmDomain",
 					"l3ExtDomain",
@@ -69,7 +63,7 @@ func dataSourceMSOSchemaSiteAnpEpgDomain() *schema.Resource {
 			"vmm_domain_type": &schema.Schema{
 				Type:     schema.TypeString,
 				Optional: true,
-				ForceNew: true,
+				Computed: true,
 				ValidateFunc: validation.StringInSlice([]string{
 					"VMware",
 					"Microsoft",
@@ -79,76 +73,60 @@ func dataSourceMSOSchemaSiteAnpEpgDomain() *schema.Resource {
 			"domain_dn": &schema.Schema{
 				Type:          schema.TypeString,
 				Optional:      true,
-				ForceNew:      true,
+				Computed:      true,
 				ValidateFunc:  validation.StringLenBetween(1, 1000),
 				ConflictsWith: []string{"domain_name", "vmm_domain_type", "domain_type"},
 			},
 			"dn": &schema.Schema{
-				Type:          schema.TypeString,
-				Optional:      true,
-				ForceNew:      true,
-				ValidateFunc:  validation.StringLenBetween(1, 1000),
-				ConflictsWith: []string{"domain_name", "domain_dn"},
-				Deprecated:    "use domain_dn alone or domain_name in association with domain_type and vmm_domain_type when it is applicable.",
+				Type:     schema.TypeString,
+				Computed: true,
 			},
 			"deployment_immediacy": &schema.Schema{
 				Type:     schema.TypeString,
-				Optional: true,
-				ForceNew: true,
+				Computed: true,
 			},
 			"resolution_immediacy": &schema.Schema{
 				Type:     schema.TypeString,
-				Optional: true,
 				Computed: true,
 			},
 			"micro_seg_vlan_type": &schema.Schema{
 				Type:     schema.TypeString,
-				Optional: true,
 				Computed: true,
 			},
 			"micro_seg_vlan": &schema.Schema{
 				Type:     schema.TypeInt,
-				Optional: true,
 				Computed: true,
 			},
 			"port_encap_vlan_type": &schema.Schema{
 				Type:     schema.TypeString,
-				Optional: true,
 				Computed: true,
 			},
 			"port_encap_vlan": &schema.Schema{
 				Type:     schema.TypeInt,
-				Optional: true,
 				Computed: true,
 			},
 			"vlan_encap_mode": &schema.Schema{
 				Type:     schema.TypeString,
-				Optional: true,
 				Computed: true,
 			},
 			"allow_micro_segmentation": &schema.Schema{
 				Type:     schema.TypeBool,
-				Optional: true,
 				Computed: true,
 			},
 			"switching_mode": &schema.Schema{
 				Type:     schema.TypeString,
-				Optional: true,
 				Computed: true,
 			},
 			"switch_type": &schema.Schema{
 				Type:     schema.TypeString,
-				Optional: true,
 				Computed: true,
 			},
 			"enhanced_lag_policy_name": &schema.Schema{
 				Type:     schema.TypeString,
-				Optional: true,
 				Computed: true,
 			},
 			"enhanced_lag_policy_dn": &schema.Schema{
 				Type:     schema.TypeString,
-				Optional: true,
 				Computed: true,
 			},
 		}),
@@ -160,37 +138,27 @@ func dataSourceMSOSchemaSiteAnpEpgDomainRead(d *schema.ResourceData, m interface
 
 	msoClient := m.(*client.Client)
 	schemaId := d.Get("schema_id").(string)
-
-	cont, err := msoClient.GetViaURL(fmt.Sprintf("api/v1/schemas/%s", schemaId))
-	if err != nil {
-		return err
-	}
-	count, err := cont.ArrayCount("sites")
-	if err != nil {
-		return fmt.Errorf("No Sites found")
-	}
-	stateSite := d.Get("site_id").(string)
-	found := false
-	stateTemplate := d.Get("template_name").(string)
-	stateAnp := d.Get("anp_name").(string)
-	stateEpg := d.Get("epg_name").(string)
+	siteId := d.Get("site_id").(string)
+	templateName := d.Get("template_name").(string)
+	anp := d.Get("anp_name").(string)
+	epg := d.Get("epg_name").(string)
 	domainNameDnOld := d.Get("dn").(string)
 	domainNameNew := d.Get("domain_name").(string)
 	domainType := d.Get("domain_type").(string)
-	DN := d.Get("domain_dn").(string)
+	dn := d.Get("domain_dn").(string)
 
 	var domainName, stateDomain string
 
-	if domainNameNew == "" && DN == "" && domainNameDnOld == "" {
+	if domainNameNew == "" && dn == "" && domainNameDnOld == "" {
 		return fmt.Errorf("domain_dn or domain_name in association with domain_type and vmm_domain_type when it is applicable are required.")
 	}
 
-	if domainNameNew != "" && DN == "" && domainNameDnOld == "" {
+	if domainNameNew != "" && dn == "" && domainNameDnOld == "" {
 		domainName = domainNameNew
-	} else if domainNameNew == "" && DN == "" && domainNameDnOld != "" {
+	} else if domainNameNew == "" && dn == "" && domainNameDnOld != "" {
 		domainName = domainNameDnOld
-	} else if domainNameNew == "" && DN != "" && domainNameDnOld == "" {
-		stateDomain = DN
+	} else if domainNameNew == "" && dn != "" && domainNameDnOld == "" {
+		stateDomain = dn
 	}
 	if stateDomain == "" {
 		if domainType == "vmmDomain" {
@@ -212,120 +180,89 @@ func dataSourceMSOSchemaSiteAnpEpgDomainRead(d *schema.ResourceData, m interface
 		}
 	}
 
-	for i := 0; i < count; i++ {
-		tempCont, err := cont.ArrayElement(i, "sites")
+	siteCont, err := getSiteFromSiteIdAndTemplate(schemaId, siteId, templateName, msoClient)
+	if err != nil {
+		return err
+	} else {
+		d.Set("schema_id", schemaId)
+		d.Set("site_id", siteId)
+		d.Set("template_name", templateName)
+	}
+
+	anpCont, err := getSiteAnp(anp, siteCont)
+	if err != nil {
+		return err
+	} else {
+		d.Set("anp_name", anp)
+	}
+
+	epgCont, err := getSiteEpg(epg, anpCont)
+	if err != nil {
+		return err
+	} else {
+		d.Set("epg_name", epg)
+	}
+
+	domainCount, err := epgCont.ArrayCount("domainAssociations")
+	if err != nil {
+		return fmt.Errorf("Unable to get Domain Associations list")
+	}
+
+	found := false
+	for l := 0; l < domainCount; l++ {
+		domainCont, err := epgCont.ArrayElement(l, "domainAssociations")
 		if err != nil {
 			return err
 		}
-		apiSite := models.StripQuotes(tempCont.S("siteId").String())
-		apiTemplate := models.StripQuotes(tempCont.S("templateName").String())
+		domain := models.StripQuotes(domainCont.S("dn").String())
 
-		if apiSite == stateSite && apiTemplate == stateTemplate {
-			anpCount, err := tempCont.ArrayCount("anps")
-			if err != nil {
-				return fmt.Errorf("Unable to get Anp list")
+		if domain == stateDomain {
+			found = true
+			d.SetId(fmt.Sprintf("%s/sites/%s-%s/anps/%s/epgs/%s/domainAssociations/%s", schemaId, siteId, templateName, anp, epg, stateDomain))
+			d.Set("domain_type", models.StripQuotes(domainCont.S("domainType").String()))
+			d.Set("domain_dn", stateDomain)
+			d.Set("deploy_immediacy", models.StripQuotes(domainCont.S("deployImmediacy").String()))
+			d.Set("resolution_immediacy", models.StripQuotes(domainCont.S("resolutionImmediacy").String()))
+
+			if domainCont.Exists("switchingMode") {
+				d.Set("switching_mode", models.StripQuotes(domainCont.S("switchingMode").String()))
 			}
-			for j := 0; j < anpCount; j++ {
-				anpCont, err := tempCont.ArrayElement(j, "anps")
-				if err != nil {
-					return err
-				}
-				apiAnpRef := models.StripQuotes(anpCont.S("anpRef").String())
-				split := strings.Split(apiAnpRef, "/")
-				apiAnp := split[6]
-				if apiAnp == stateAnp {
-					epgCount, err := anpCont.ArrayCount("epgs")
-					if err != nil {
-						return fmt.Errorf("Unable to get EPG list")
-					}
-					for k := 0; k < epgCount; k++ {
-						epgCont, err := anpCont.ArrayElement(k, "epgs")
-						if err != nil {
-							return err
-						}
-						apiEpgRef := models.StripQuotes(epgCont.S("epgRef").String())
-						split := strings.Split(apiEpgRef, "/")
-						apiEPG := split[8]
-						if apiEPG == stateEpg {
-							d.Set("schema_id", split[2])
-							d.Set("template_name", split[4])
-							d.Set("anp_name", split[6])
-							d.Set("epg_name", apiEPG)
 
-							domainCount, err := epgCont.ArrayCount("domainAssociations")
-							if err != nil {
-								return fmt.Errorf("Unable to get Domain Associations list")
-							}
-							for l := 0; l < domainCount; l++ {
-								domainCont, err := epgCont.ArrayElement(l, "domainAssociations")
-								if err != nil {
-									return err
-								}
-								apiDomain := models.StripQuotes(domainCont.S("dn").String())
+			if domainCont.Exists("switchType") {
+				d.Set("switch_type", models.StripQuotes(domainCont.S("switchType").String()))
+			}
 
-								if apiDomain == stateDomain {
-									d.SetId(fmt.Sprintf("%s/sites/%s-%s/anps/%s/epgs/%s/domainAssociations/%s", schemaId, apiSite, apiTemplate, apiAnp, apiEPG, apiDomain))
-									d.Set("site_id", apiSite)
-									d.Set("domain_type", models.StripQuotes(domainCont.S("domainType").String()))
-									d.Set("domain_dn", apiDomain)
+			if domainCont.Exists("vlanEncapMode") {
+				d.Set("vlan_encap_mode", models.StripQuotes(domainCont.S("vlanEncapMode").String()))
+			}
 
-									d.Set("deploy_immediacy", models.StripQuotes(domainCont.S("deployImmediacy").String()))
-									d.Set("resolution_immediacy", models.StripQuotes(domainCont.S("resolutionImmediacy").String()))
+			if domainCont.Exists("allowMicroSegmentation") {
+				d.Set("allow_micro_segmentation", domainCont.S("allowMicroSegmentation").Data().(bool))
+			}
 
-									if domainCont.Exists("switchingMode") {
-										d.Set("switching_mode", models.StripQuotes(domainCont.S("switchingMode").String()))
-									}
+			if domainCont.Exists("portEncapVlan") {
+				d.Set("port_encap_vlan", domainCont.S("portEncapVlan", "vlan").Data().(float64))
+				d.Set("port_encap_vlan_type", models.StripQuotes(domainCont.S("portEncapVlan", "vlanType").String()))
+			}
 
-									if domainCont.Exists("switchType") {
-										d.Set("switch_type", models.StripQuotes(domainCont.S("switchType").String()))
-									}
+			if domainCont.Exists("microSegVlan") {
+				d.Set("micro_seg_vlan", domainCont.S("microSegVlan", "vlan").Data().(float64))
+				d.Set("micro_seg_vlan_type", models.StripQuotes(domainCont.S("microSegVlan", "vlanType").String()))
+			}
 
-									if domainCont.Exists("vlanEncapMode") {
-										d.Set("vlan_encap_mode", models.StripQuotes(domainCont.S("vlanEncapMode").String()))
-									}
-
-									if domainCont.Exists("allowMicroSegmentation") {
-										d.Set("allow_micro_segmentation", domainCont.S("allowMicroSegmentation").Data().(bool))
-									}
-
-									if domainCont.Exists("portEncapVlan") {
-										d.Set("port_encap_vlan", domainCont.S("portEncapVlan", "vlan").Data().(float64))
-										d.Set("port_encap_vlan_type", models.StripQuotes(domainCont.S("portEncapVlan", "vlanType").String()))
-									}
-
-									if domainCont.Exists("microSegVlan") {
-										d.Set("micro_seg_vlan", domainCont.S("microSegVlan", "vlan").Data().(float64))
-										d.Set("micro_seg_vlan_type", models.StripQuotes(domainCont.S("microSegVlan", "vlanType").String()))
-									}
-
-									if domainCont.Exists("epgLagPol") {
-										if domainCont.Exists("epgLagPol", "enhancedLagPol") {
-											d.Set("enhanced_lag_policy_name", models.StripQuotes(domainCont.S("epgLagPol", "enhancedLagPol", "name").String()))
-											d.Set("enhanced_lag_policy_dn", models.StripQuotes(domainCont.S("epgLagPol", "enhancedLagPol", "dn").String()))
-										}
-									}
-									found = true
-									break
-								}
-							}
-						}
-						if found {
-							break
-						}
-					}
-				}
-				if found {
-					break
+			if domainCont.Exists("epgLagPol") {
+				if domainCont.Exists("epgLagPol", "enhancedLagPol") {
+					d.Set("enhanced_lag_policy_name", models.StripQuotes(domainCont.S("epgLagPol", "enhancedLagPol", "name").String()))
+					d.Set("enhanced_lag_policy_dn", models.StripQuotes(domainCont.S("epgLagPol", "enhancedLagPol", "dn").String()))
 				}
 			}
-		}
-		if found {
 			break
 		}
 	}
 
 	if !found {
-		return fmt.Errorf("Unable to find the Site Anp Epg Domain %s", stateDomain)
+		d.SetId("")
+		return fmt.Errorf("Unable to find the Site ANP EPG Domain %s", stateDomain)
 	}
 
 	log.Printf("[DEBUG] %s: Read finished successfully", d.Id())
--- website/docs/d/schema_site_anp_epg_domain.html.markdown
+++ website/docs/d/schema_site_anp_epg_domain.html.markdown
@@ -3,12 +3,12 @@ layout: "mso"
 page_title: "MSO: mso_schema_site_anp_epg_domain"
 sidebar_current: "docs-mso-data-source-schema_site_anp_epg_domain"
 description: |-
-  Data source for MSO Schema Site Application Network Profiles Endpoint Groups Domain.
+  Data source for MSO Schema Site Application Network Profiles End Point Group Domain.
 ---
 
 # mso_schema_site_anp_epg_domain #
 
-Data source for MSO Schema Site Application Network Profiles Endpoint Groups Domain.
+Data source for MSO Schema Site Application Network Profiles End Point Group Domain.
 
 ## Example Usage ##
 
@@ -16,7 +16,7 @@ Data source for MSO Schema Site Application Network Profiles Endpoint Groups Dom
 
 ```hcl
 
-data "mso_schema_site_anp_epg_domain" "anpEpgDomain" {
+data "mso_schema_site_anp_epg_domain" "example_name" {
   schema_id       = data.mso_schema.schema1.id
   site_id         = data.mso_site.site1.id
   template_name   = "Template1"
@@ -32,42 +32,42 @@ data "mso_schema_site_anp_epg_domain" "anpEpgDomain" {
 ### domain_dn usage ###
 
 ```hcl
-resource "mso_schema_site_anp_epg_domain" "site_anp_epg_domain" {
+
+resource "mso_schema_site_anp_epg_domain" "example_dn" {
   schema_id = "5c4d9fca270000a101f8094a"
   template_name = "Template1"
   site_id = "5c7c95b25100008f01c1ee3c"
   anp_name = "ANP"
   epg_name = "Web"
   domain_dn = "uni/vmmp-VMware/dom-VMware-ab"
-  
 }
 
 ```
 
 ## Argument Reference ##
 
-* `schema_id` - (Required) SchemaID under which you want to deploy Anp Epg Domain.
-* `site_id` - (Required) SiteID under which you want to deploy Anp Epg Domain.
-* `anp_name` - (Required) Name of Application Network Profiles.
-* `epg_name` - (Required) Name of Endpoint Group to manage.
-* `dn` - (Optional) **Deprecated**. The domain profile name. Use `domain_dn` or `domain_name` in association with `domain_type` and `vmm_domain_type` when it is applicable instead.
-* `domain_dn` - (Optional) The dn of domain. This is required when `domain_name` and `domain_type` are not specified.
-* `domain_name` - (Optional) The domain profile name. This is required when `domain_dn` is not used. This attribute requires `domain_type` and `vmm_domain_type` (when it is applicable) to be set.
-* `domain_type` - (Optional) The type of domain to associate. This is required when `domain_dn` is not used. Choices: [ vmmDomain, l3ExtDomain, l2ExtDomain, physicalDomain, fibreChannelDomain ]
-* `vmm_domain_type` - (Optional) The vmm domain type. This is required when `domain_type` is vmmDomain and `domain_dn` is not used. Choices: [ VMware, Microsoft, Redhat ]
+* `schema_id` - (Required) The schema ID under which the Domain is deployed.
+* `site_id` - (Required) The site ID under which the Domain is deployed.
+* `template_name` - (Required) The template name under which the Domain is deployed.
+* `anp_name` - (Required) The ANP name under which the Domain is deployed.
+* `epg_name` - (Required) The EPG name under which the Domain is deployed.
+* `domain_dn` - (Optional) The DN of the Domain. This is required when `domain_name` and `domain_type` are not specified.
+* `domain_name` - (Optional) The name of the Domain. This is required when `domain_dn` is not used. This attribute requires `domain_type` and `vmm_domain_type` (when it is applicable) to be set.
+* `domain_type` - (Optional)  The type of the Domain. This is required when `domain_dn` is not used. Choices: [ vmmDomain, l3ExtDomain, l2ExtDomain, physicalDomain, fibreChannelDomain ]
+* `vmm_domain_type` - (Optional) The type of the VMM Domain. This is required when `domain_type` is vmmDomain and `domain_dn` is not used. Choices: [ VMware, Microsoft, Redhat ]
 
 ## Attribute Reference ##
 
-* `template_name` - (Optional) Template where Anp Epg Domain to be created.
-* `deploy_immediacy` - (Optional) The deployment immediacy of the domain. choices: [ immediate, lazy ]
-* `resolution_immediacy` - (Optional) Determines when the policies should be resolved and available. choices: [ immediate, lazy, pre-provision ]
-* `vlan_encap_mode` - (Optional) Which VLAN enacap mode to use. This attribute can only be used with vmmDomain domain association. choices: [ static, dynamic ]
-* `allow_micro_segmentation` - (Optional) Specifies microsegmentation is enabled or not. This attribute can only be used with vmmDomain domain association.
-* `switching_mode` - (Optional) Which switching mode to use with this domain association. This attribute can only be used with vmmDomain domain association.
-* `switch_type` - (Optional) Which switch type to use with this domain association. This attribute can only be used with vmmDomain domain association.
-* `micro_seg_vlan_type` - (Optional) Virtual LAN type for microsegmentation. This attribute can only be used with vmmDomain domain association.
-* `micro_seg_vlan` - (Optional) Virtual LAN for microsegmentation. This attribute can only be used with vmmDomain domain association.
-* `port_encap_vlan_type` - (Optional) Virtual LAN type for port encap. This attribute can only be used with vmmDomain domain association.
-* `port_encap_vlan` - (Optional) Virtual LAN for port encap. This attribute can only be used with vmmDomain domain association.
-* `enhanced_lag_policy_name` - (Optional) EPG enhanced lagpolicy name. This attribute can only be used with vmmDomain domain association.
-* `enhanced_lag_policy_dn` - (Optional) Distinguished name of EPG lagpolicy. This attribute can only be used with vmmDomain domain association.
+* `template_name` - (Read-Only) The template of the Domain.
+* `deploy_immediacy` - (Read-Only) The deployment immediacy of the Domain.
+* `resolution_immediacy` - (Read-Only) The resolution immediacy of the Domain.
+* `vlan_encap_mode` - (Read-Only) The VLAN encapsulation mode of the Domain.
+* `allow_micro_segmentation` - (Read-Only) The allow microsegmentation of the Domain.
+* `switching_mode` - (Read-Only) The switching mode of the Domain. 
+* `switch_type` - (Read-Only) The switch type of the Domain.
+* `micro_seg_vlan_type` - (Read-Only) The virtual LAN type for microsegmentation of the Domain. 
+* `micro_seg_vlan` - (Read-Only) The virtual LAN for microsegmentation of the Domain. 
+* `port_encap_vlan_type` - (Read-Only) The virtual LAN type for port encapsulation of the Domain.
+* `port_encap_vlan` - (Read-Only) The port encapapsulation of the Domain.
+* `enhanced_lag_policy_name` - (Read-Only) The EPG enhanced lag policy name of the Domain.
+* `enhanced_lag_policy_dn` - (Read-Only) The EPG enhanced lag policy DN of the Domain. 
