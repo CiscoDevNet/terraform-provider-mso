a10d761b1285f88bc4db2eae2ab4dd07e7af5e3d
akinross <akinross@cisco.com>
2023-08-03 11:13:37 +0200

    [ignore] update documentation for schema_template_bd datasource to be consistent with other datasource documentation

--- mso/datasource_mso_schema_template_bd.go
+++ mso/datasource_mso_schema_template_bd.go
@@ -1,6 +1,14 @@
 package mso
 
 import (
+	"fmt"
+	"log"
+	"regexp"
+	"strconv"
+
+	"github.com/ciscoecosystem/mso-go-client/client"
+	"github.com/ciscoecosystem/mso-go-client/container"
+	"github.com/ciscoecosystem/mso-go-client/models"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 )
@@ -146,5 +154,201 @@ func dataSourceMSOTemplateBD() *schema.Resource {
 }
 
 func dataSourceMSOTemplateBDRead(d *schema.ResourceData, m interface{}) error {
-	return resourceMSOTemplateBDRead(d, m)
+	log.Printf("[DEBUG] %s: Beginning Read", d.Id())
+
+	msoClient := m.(*client.Client)
+
+	schemaId := d.Get("schema_id").(string)
+
+	cont, err := msoClient.GetViaURL(fmt.Sprintf("api/v1/schemas/%s", schemaId))
+	if err != nil {
+		return err
+	}
+
+	templateName := d.Get("template_name").(string)
+	bdName := d.Get("name").(string)
+
+	err = setSchemaTemplateBDAttrs(schemaId, templateName, bdName, cont, d, msoClient)
+
+	if err != nil {
+		return err
+	}
+
+	log.Printf("[DEBUG] %s: Read finished successfully", d.Id())
+	return nil
+}
+
+func setSchemaTemplateBDAttrs(schemaId, templateName, bdName string, cont *container.Container, d *schema.ResourceData, msoClient *client.Client) error {
+	count, err := cont.ArrayCount("templates")
+	if err != nil {
+		return fmt.Errorf("No Template found")
+	}
+
+	versionInt, err := msoClient.CompareVersion("4.0.0.0")
+	if err != nil {
+		return err
+	}
+
+	found := false
+	for i := 0; i < count && !found; i++ {
+		tempCont, err := cont.ArrayElement(i, "templates")
+		if err != nil {
+			return err
+		}
+		apiTemplate := models.StripQuotes(tempCont.S("name").String())
+		if apiTemplate == templateName {
+			bdCount, err := tempCont.ArrayCount("bds")
+			if err != nil {
+				return fmt.Errorf("Unable to get BD list")
+			}
+			for j := 0; j < bdCount; j++ {
+				bdCont, err := tempCont.ArrayElement(j, "bds")
+				if err != nil {
+					return err
+				}
+				apiBD := models.StripQuotes(bdCont.S("name").String())
+				if apiBD == bdName {
+					found = true
+					d.SetId(fmt.Sprintf("%s/templates/%s/bds/%s", schemaId, templateName, bdName))
+					d.Set("name", bdName)
+					d.Set("schema_id", schemaId)
+					d.Set("template_name", templateName)
+					d.Set("display_name", models.StripQuotes(bdCont.S("displayName").String()))
+					d.Set("description", models.StripQuotes(bdCont.S("description").String()))
+					d.Set("layer2_unknown_unicast", models.StripQuotes(bdCont.S("l2UnknownUnicast").String()))
+					if models.StripQuotes(bdCont.S("unkMcastAct").String()) == "opt-flood" {
+						d.Set("unknown_multicast_flooding", "optimized_flooding")
+					} else {
+						d.Set("unknown_multicast_flooding", "flood")
+					}
+					multiDstPktAct := models.StripQuotes(bdCont.S("multiDstPktAct").String())
+					if multiDstPktAct == "encap-flood" {
+						d.Set("multi_destination_flooding", "flood_in_encap")
+					} else if multiDstPktAct == "bd-flood" {
+						d.Set("multi_destination_flooding", "flood_in_bd")
+					} else {
+						d.Set("multi_destination_flooding", "drop")
+					}
+					v6unkMcastAct := models.StripQuotes(bdCont.S("v6unkMcastAct").String())
+					if v6unkMcastAct == "opt-flood" {
+						d.Set("ipv6_unknown_multicast_flooding", "optimized_flooding")
+					} else {
+						d.Set("ipv6_unknown_multicast_flooding", "flood")
+					}
+
+					vmac := models.StripQuotes(bdCont.S("vmac").String())
+					if vmac != "{}" {
+						d.Set("virtual_mac_address", vmac)
+					} else {
+						d.Set("virtual_mac_address", "")
+					}
+
+					if bdCont.Exists("intersiteBumTrafficAllow") {
+						d.Set("intersite_bum_traffic", bdCont.S("intersiteBumTrafficAllow").Data().(bool))
+					}
+
+					if bdCont.Exists("optimizeWanBandwidth") {
+						d.Set("optimize_wan_bandwidth", bdCont.S("optimizeWanBandwidth").Data().(bool))
+					}
+
+					if bdCont.Exists("l3MCast") {
+						d.Set("layer3_multicast", bdCont.S("l3MCast").Data().(bool))
+					}
+
+					if bdCont.Exists("l2Stretch") {
+						d.Set("layer2_stretch", bdCont.S("l2Stretch").Data().(bool))
+					}
+					if bdCont.Exists("arpFlood") {
+						d.Set("arp_flooding", bdCont.S("arpFlood").Data().(bool))
+					}
+					if bdCont.Exists("unicastRouting") {
+						d.Set("unicast_routing", bdCont.S("unicastRouting").Data().(bool))
+					}
+
+					vrfRef := models.StripQuotes(bdCont.S("vrfRef").String())
+					re := regexp.MustCompile("/schemas/(.*)/templates/(.*)/vrfs/(.*)")
+					match := re.FindStringSubmatch(vrfRef)
+					d.Set("vrf_name", match[3])
+					d.Set("vrf_schema_id", match[1])
+					d.Set("vrf_template_name", match[2])
+
+					dhcpPolMap := make(map[string]interface{})
+					dhcpPoliciesList := make([]interface{}, 0)
+
+					var dhcpCount int
+					if bdCont.Exists("dhcpLabels") {
+						dhcpCount, err = bdCont.ArrayCount("dhcpLabels")
+						if err != nil {
+							return err
+						}
+					} else {
+						dhcpCount = 0
+					}
+
+					if dhcpCount != 0 {
+						if versionInt == -1 {
+							dhcpPoliciesNameList, error := getDHCPPolicesNameByRef(dhcpCount, schemaId, templateName, bdCont, msoClient)
+							if error != nil {
+								return error
+							}
+							dhcpPoliciesList = dhcpPoliciesNameList
+						} else {
+							for l := 0; l < dhcpCount; l++ {
+								dhcpPolicy, err := bdCont.ArrayElement(l, "dhcpLabels")
+								if err != nil {
+									return err
+								}
+								dhcpPolicyMap := make(map[string]interface{})
+								dhcpPolicyMap["name"] = models.StripQuotes(dhcpPolicy.S("name").String())
+								var version int
+								if dhcpPolicy.Exists("version") {
+									version, err = strconv.Atoi(models.StripQuotes(dhcpPolicy.S("version").String()))
+								}
+								if err != nil {
+									return err
+								}
+								dhcpPolicyMap["version"] = version
+								if dhcpPolicy.Exists("dhcpOptionLabel") {
+									dhcpPolicyMap["dhcp_option_policy_name"] = models.StripQuotes(dhcpPolicy.S("dhcpOptionLabel", "name").String())
+									version, err := strconv.Atoi(models.StripQuotes(dhcpPolicy.S("dhcpOptionLabel", "version").String()))
+									if err != nil {
+										return err
+									}
+									dhcpPolicyMap["dhcp_option_policy_version"] = version
+								}
+								dhcpPoliciesList = append(dhcpPoliciesList, dhcpPolicyMap)
+							}
+						}
+					} else {
+						if bdCont.Exists("dhcpLabel") {
+							dhcpPolMap["name"] = models.StripQuotes(bdCont.S("dhcpLabel", "name").String())
+							dhcpPolMap["version"] = models.StripQuotes(bdCont.S("dhcpLabel", "version").String())
+							if dhcpPolMap["version"] == "{}" {
+								dhcpPolMap["version"] = nil
+							}
+							if bdCont.Exists("dhcpLabel", "dhcpOptionLabel") {
+								dhcpPolMap["dhcp_option_policy_name"] = models.StripQuotes(bdCont.S("dhcpLabel", "dhcpOptionLabel", "name").String())
+								dhcpPolMap["dhcp_option_policy_version"] = models.StripQuotes(bdCont.S("dhcpLabel", "dhcpOptionLabel", "version").String())
+								if dhcpPolMap["dhcp_option_policy_name"] == "" {
+									dhcpPolMap["dhcp_option_policy_name"] = nil
+								}
+								if dhcpPolMap["dhcp_option_policy_version"] == "{}" {
+									dhcpPolMap["dhcp_option_policy_version"] = nil
+								}
+							}
+						}
+					}
+					d.Set("dhcp_policy", dhcpPolMap)
+					d.Set("dhcp_policies", dhcpPoliciesList)
+					break
+				}
+			}
+		}
+	}
+
+	if !found {
+		return fmt.Errorf("Unable to find the BD %s in Template %s of Schema Id %s ", bdName, templateName, schemaId)
+	}
+
+	return nil
 }
--- website/docs/d/schema_template_bd.html.markdown
+++ website/docs/d/schema_template_bd.html.markdown
@@ -3,18 +3,18 @@ layout: "mso"
 page_title: "MSO: mso_schema_template_bd"
 sidebar_current: "docs-mso-data-source-schema_template_bd"
 description: |-
-  MSO Schema Template Bridge Domain Data Source.
+  Data source for MSO Schema Template Bridge Domain (BD).
 ---
 
 # mso_schema_template_bd #
 
-MSO Schema Template Bridge Domain Data source.
+Data source for MSO Schema Template Bridge Domain (BD).
 
 ## Example Usage ##
 
 ```hcl
 
-data "mso_schema_template_bd" "bridge_domain" {
+data "mso_schema_template_bd" "example" {
     schema_id              = data.mso_schema.schema1.id
     template_name          = "Template1"
     name                   = "testBD"
@@ -24,40 +24,37 @@ data "mso_schema_template_bd" "bridge_domain" {
 
 ## Argument Reference ##
 
-* `schema_id` - (Required) SchemaID of Bridge Domain.
-* `template_name` - (Required) Template name of Bridge Domain.
-* `name` - (Required) Name of Bridge Domain.
-
+* `schema_id` - (Required) The schema ID of the BD.
+* `template_name` - (Required) The template name of the BD.
+* `name` - (Required) The name of the BD.
 
 ## Attribute Reference ##
-* `display_name` - (Read-Only) Display Name of the Bridge Domain on the MSO UI.
-* `description` - (Read-Only) Description of the Bridge Domain on the MSO UI.
-* `vrf_name` - (Read-Only) Name of the VRF attached with Bridge Domain on the MSO UI.
-* `vrf_schema_id` - (Read-Only) SchemaID of the VRF.
-* `vrf_template_name` - (Read-Only) Template Name of the VRF.
-* `layer2_unknown_unicast` - (Read-Only) Type of the layer 2 unknown unicast.
-* `intersite_bum_traffic` - (Read-Only) Enable or Disable - boolean flag of the intersite bum traffic.
-* `optimize_wan_bandwidth` - (Read-Only) Enable or Disable - boolean flag of the wan bandwidth optimization.
-* `layer2_stretch` - (Read-Only) Enable or Disable - boolean flag of the layer-2 stretch.
-* `layer3_multicast` - (Read-Only) Enable or Disable - boolean flag of the layer 3 multicast traffic.
-* `dhcp_policies` - (Read-Only) Block to provide dhcp_policy configurations. Type: Block.
-  * `name` - (Read-Only) DHCP Policy name of the Bridge Domain on the MSO UI.
-  * `version` - (Read-Only) DHCP Policy version of the Bridge Domain on the MSO UI.
-  * `dhcp_option_policy_name` - (Read-Only) DHCP Option Policy name of the Bridge Domain on the MSO UI.
-  * `dhcp_option_policy_version` - (Read-Only) DHCP Option Policy version of the Bridge Domain on the MSO UI.
-* `unknown_multicast_flooding` - (Read-Only) Unknown Multicast flooding of the Bridge Domain on the MSO UI.
-* `multi_destination_flooding` - (Read-Only) Multi destination flooding of the Bridge Domain on the MSO UI.
-* `ipv6_unknown_multicast_flooding` - (Read-Only) IPv6 unknown multicast flooding of the Bridge Domain on the MSO UI.
-* `arp_flooding` - (Read-Only) ARP flooding of the Bridge Domain on the MSO UI.
-* `virtual_mac_address` - (Read-Only) Virtual Mac Address of the Bridge Domain on the MSO UI.
-* `unicast_routing` - (Read-Only) Unicast Routing of the Bridge Domain on the MSO UI.
-
-			
+* `display_name` - (Read-Only) The name of the BD as displayed on the MSO UI.
+* `description` - (Read-Only) The description of the BD.
+* `vrf_name` - (Read-Only) The name of the VRF associated with the BD.
+* `vrf_schema_id` - (Read-Only) The schema ID of the VRF associated with the BD.
+* `vrf_template_name` - (Read-Only) The template name of the VRF associated with the BD.
+* `layer2_unknown_unicast` - (Read-Only) The layer 2 unknown unicast type of the BD.
+* `intersite_bum_traffic` - (Read-Only) Whether intersite bum traffic is enabled.
+* `optimize_wan_bandwidth` - (Read-Only)  Whether wan bandwidth optimization is enabled.
+* `layer2_stretch` - (Read-Only) Whether layer-2 stretch is enabled.
+* `layer3_multicast` - (Read-Only) Whether layer 3 multicast traffic is enabled.
+* `dhcp_policies` - (Read-Only) A list of dhcp_policy configurations for the BD.
+    * `name` - (Read-Only) The DHCP policy name of the BD.
+    * `version` - (Read-Only) The DHCP policy version of the BD.
+    * `dhcp_option_policy_name` - (Read-Only) The DHCP option policy name of the BD.
+    * `dhcp_option_policy_version` - (Read-Only) The DHCP option policy version of the BD.
+* `unknown_multicast_flooding` - (Read-Only) The unknown multicast flooding settings of the BD.
+* `multi_destination_flooding` - (Read-Only) The multi destination flooding settings of the BD.
+* `ipv6_unknown_multicast_flooding` - (Read-Only) The IPv6 unknown multicast flooding settings of the BD.
+* `arp_flooding` - (Read-Only) The ARP flooding settings of the BD.
+* `virtual_mac_address` - (Read-Only) The virtual mac address of the BD.
+* `unicast_routing` - (Read-Only) Whether unicast routing is enabled.
 
 ### Deprecation warning: do not use 'dhcp_policy' map below in combination with NDO releases 3.2 and higher, use above 'dhcp_policies' block instead.
 
-* `dhcp_policy` - (Read-Only) Map to provide dhcp_policy configurations. Type: Block.
-  * `name` - (Read-Only) DHCP Policy name of the Bridge Domain on the MSO UI.
-  * `version` - (Read-Only) DHCP Policy version of the Bridge Domain on the MSO UI.
-  * `dhcp_option_policy_name` - (Read-Only) DHCP Option Policy name of the Bridge Domain on the MSO UI.
-  * `dhcp_option_policy_version` - (Read-Only) DHCP Option Policy version of the Bridge Domain on the MSO UI.
+* `dhcp_policy` - (Read-Only) A map to provide the dhcp_policy configuration.
+    * `name` - (Read-Only) The DHCP policy name of the BD.
+    * `version` - (Read-Only) The DHCP policy version of the BD.
+    * `dhcp_option_policy_name` - (Read-Only) The DHCP option policy name of the BD.
+    * `dhcp_option_policy_version` - (Read-Only) The DHCP option policy version of the BD.
