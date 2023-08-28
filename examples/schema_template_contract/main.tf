fbde0dc2ff68a29d2934167dac2541f3965180ea
akinross <akinross@cisco.com>
2023-08-28 12:33:49 +0200

    [ignore] update example with correct tenant and schema configuration in example

--- examples/schema_template_contract/main.tf
+++ examples/schema_template_contract/main.tf
@@ -13,39 +13,34 @@ provider "mso" {
   insecure = true
 }
 
-resource "mso_tenant" "tenant1" {
-  name         = "test_tenant"
-  display_name = "test_tenant"
-  description  = "DemoTenant"
-  site_associations {
-    site_id = mso_site.test_site.id
+data "mso_user" "user" {
+  username = "admin"
+}
+
+resource "mso_tenant" "demo_tenant" {
+  name         = "demo_tenant"
+  display_name = "demo_tenant"
+  user_associations {
+    user_id = data.mso_user.user.id
   }
 }
 
-resource "mso_schema" "schema1" {
-  name          = "test_schema"
-  template_name = "Template1"
-  tenant_id     = mso_tenant.tenant1.id
+resource "mso_schema" "demo_schema" {
+  name = "demo_schema"
+  template {
+    name         = "Template1"
+    display_name = "Template1"
+    tenant_id    = mso_tenant.demo_tenant.id
+  }
 }
 
 resource "mso_schema_template_filter_entry" "filter_entry" {
-  schema_id            = mso_schema.schema1.id
-  template_name        = mso_schema.schema1.template_name
+  schema_id            = mso_schema.demo_schema.id
+  template_name        = one(mso_schema.demo_schema.template).name
   name                 = "Filter1"
   display_name         = "Filter1"
   entry_name           = "entry1"
   entry_display_name   = "entry1"
-  entry_description    = "DemoEntry"
-  ether_type           = "arp"
-  ip_protocol          = "eigrp"
-  tcp_session_rules    = ["acknowledgement"]
-  destination_from     = "unspecified"
-  destination_to       = "unspecified"
-  source_from          = "unspecified"
-  source_to            = "unspecified"
-  arp_flag             = "unspecified"
-  stateful             = false
-  match_only_fragments = false
 }
 
 resource "mso_schema_template_filter_entry" "filter_entry_2" {
@@ -55,22 +50,11 @@ resource "mso_schema_template_filter_entry" "filter_entry_2" {
   display_name         = "Filter2"
   entry_name           = "entry2"
   entry_display_name   = "entry2"
-  entry_description    = "DemoEntry"
-  ether_type           = "arp"
-  ip_protocol          = "eigrp"
-  tcp_session_rules    = ["acknowledgement"]
-  destination_from     = "unspecified"
-  destination_to       = "unspecified"
-  source_from          = "unspecified"
-  source_to            = "unspecified"
-  arp_flag             = "unspecified"
-  stateful             = false
-  match_only_fragments = false
 }
 
 resource "mso_schema_template_contract" "template_contract" {
-  schema_id     = mso_schema_template_filter_entry.filter_entry_2.schema_id
-  template_name = mso_schema_template_filter_entry.filter_entry_2.template_name
+  schema_id     = mso_schema_template_filter_entry.filter_entry.schema_id
+  template_name = mso_schema_template_filter_entry.filter_entry.template_name
   contract_name = "Contract1"
   display_name  = "Contract1"
   filter_type   = "bothWay"
@@ -78,8 +62,6 @@ resource "mso_schema_template_contract" "template_contract" {
   priority      = "level1"
   scope         = "context"
   filter_relationship {
-    filter_schema_id     = mso_schema_template_filter_entry.filter_entry.schema_id
-    filter_template_name = mso_schema_template_filter_entry.filter_entry.template_name
     filter_name          = mso_schema_template_filter_entry.filter_entry.name
     filter_type          = "bothWay"
     action               = "permit"
@@ -109,7 +91,7 @@ resource "mso_schema_template_contract" "template_contract" {
   filter_relationships = {
     filter_schema_id     = mso_schema_template_filter_entry.filter_entry_2.schema_id
     filter_template_name = mso_schema_template_filter_entry.filter_entry_2.template_name
-    filter_name          = mso_schema_template_filter_entry.filter_entry.name
+    filter_name          = mso_schema_template_filter_entry.filter_entry_2.name
   }
   directives = ["none"]
 }
