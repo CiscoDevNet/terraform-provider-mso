e91b479a82aedf7e944d8c171278cca3f5f1d7f9
anvitha-jain <anvithjain@gmail.com>
2023-09-06 10:05:39 -0700

    [minor_changes] Made deprecated argument available.

--- mso/datasource_mso_schema_template_service_graph.go
+++ mso/datasource_mso_schema_template_service_graph.go
@@ -5,7 +5,6 @@ import (
 	"log"
 
 	"github.com/ciscoecosystem/mso-go-client/client"
-	"github.com/ciscoecosystem/mso-go-client/container"
 	"github.com/ciscoecosystem/mso-go-client/models"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
@@ -35,11 +34,11 @@ func dataSourceMSOSchemaTemplateServiceGraph() *schema.Resource {
 				ValidateFunc: validation.StringLenBetween(1, 1000),
 			},
 			"service_node_type": &schema.Schema{
-				Type:         schema.TypeString,
-				Optional:     true,
-				ValidateFunc: validation.StringLenBetween(1, 1000),
-				// ConflictsWith: []string{"service_node"},
-				Deprecated: "Use service_node to configure service nodes.",
+				Type:          schema.TypeString,
+				Optional:      true,
+				ValidateFunc:  validation.StringLenBetween(1, 1000),
+				ConflictsWith: []string{"service_node"},
+				Deprecated:    "Use service_node to configure service nodes.",
 			},
 			"service_node": &schema.Schema{
 				Type:        schema.TypeList,
@@ -86,50 +85,31 @@ func dataSourceMSOSchemaTemplateServiceGrapRead(d *schema.ResourceData, m interf
 		return err
 	}
 
-	serviceNodes := sgCont.S("serviceNodes").Data().([]interface{})
+	if tempVar, ok := d.GetOk("service_node_type"); ok {
+		serviceNodeType := tempVar.(string)
+		d.Set("service_node_type", serviceNodeType)
+	} else {
+		serviceNodeList := make([]interface{}, 0, 1)
+		serviceNodes := sgCont.S("serviceNodes").Data().([]interface{})
+		for _, val := range serviceNodes {
+			serviceNodeValues := val.(map[string]interface{})
+			serviceNodeMap := make(map[string]interface{})
+			nodeId := models.StripQuotes(serviceNodeValues["serviceNodeTypeId"].(string))
 
-	serviceNodeList := make([]interface{}, 0, 1)
-	for _, val := range serviceNodes {
-		serviceNodeValues := val.(map[string]interface{})
-		serviceNodeMap := make(map[string]interface{})
-		nodeId := models.StripQuotes(serviceNodeValues["serviceNodeTypeId"].(string))
-		nodeType, err := getNodeNameFromId(msoClient, nodeId)
-		if err != nil {
-			return err
+			nodeType, err := getNodeNameFromId(msoClient, nodeId)
+			if err != nil {
+				return err
+			}
+			serviceNodeMap["type"] = nodeType
+
+			serviceNodeList = append(serviceNodeList, serviceNodeMap)
 		}
-		serviceNodeMap["type"] = nodeType
-
-		serviceNodeList = append(serviceNodeList, serviceNodeMap)
+		d.Set("service_node", serviceNodeList)
 	}
-	d.Set("service_node", serviceNodeList)
 
 	d.Set("schema_id", schemaId)
 	d.Set("template_name", stateTemplate)
 	d.Set("service_graph_name", graphName)
 	d.SetId(fmt.Sprintf("%s/templates/%s/serviceGraphs/%s", schemaId, stateTemplate, graphName))
 	return nil
-}
-
-func getTemplateServiceNodeContFromIndex(cont *container.Container, ind int) (*container.Container, int, error) {
-
-	nodeCount, err := cont.ArrayCount("serviceNodes")
-	if err != nil {
-		return nil, -1, fmt.Errorf("Unable to load node count")
-	}
-
-	for i := 0; i < nodeCount; i++ {
-		nodeCont, err := cont.ArrayElement(i, "serviceNodes")
-		if err != nil {
-			return nil, -1, fmt.Errorf("Unable to load node element")
-		}
-
-		apiInd := int(nodeCont.S("index").Data().(float64))
-
-		if apiInd == ind {
-			return nodeCont, i, nil
-		}
-
-	}
-
-	return nil, -1, fmt.Errorf("Unable to find the service node")
 }
