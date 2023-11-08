2c45c4c155348424212c688d3412c665c71584ca
Sabari Jaganathan <93724860+sajagana@users.noreply.github.com>
2023-11-08 12:28:56 +0530

    [ignore] Updated schema_site_contract_service_graph resource examples

--- examples/schema_site_contract_service_graph/main.tf
+++ examples/schema_site_contract_service_graph/main.tf
@@ -3,7 +3,17 @@ terraform {
     mso = {
       source = "CiscoDevNet/mso"
     }
+    aci = {
+      source = "ciscodevnet/aci"
+    }
   }
+}
+
+provider "aci" {
+  username = "" # <APIC username>
+  password = "" # <APIC pwd>
+  url      = "" # <cloud APIC URL>
+  insecure = true
 }
 
 provider "mso" {
@@ -11,87 +21,253 @@ provider "mso" {
   password = "" # <MSO pwd>
   url      = "" # <MSO URL>
   insecure = true
+  platform = "nd"
 }
 
+# ACI config begins
 
-data "mso_schema" "schema1" {
-  name = "schema1"
+data "aci_tenant" "ansible_test" {
+  name = "ansible_test"
 }
 
-data "mso_schema" "schema2" {
-  name = "schema2"
+data "aci_l4_l7_device" "l4_l7_fw" {
+  tenant_dn = data.aci_tenant.ansible_test.id
+  name      = "l4_l7_fw"
 }
 
-data "mso_site" "site1" {
-  name = "site1"
+data "aci_l4_l7_device" "l4_l7_adc_lb" {
+  tenant_dn = data.aci_tenant.ansible_test.id
+  name      = "l4_l7_adc_lb"
 }
 
-data "mso_schema_template" "t1" {
-  name      = "t1"
-  schema_id = data.mso_schema.schema1.id
+data "aci_l4_l7_device" "l4_l7_others" {
+  tenant_dn = data.aci_tenant.ansible_test.id
+  name      = "l4_l7_others"
 }
 
-data "mso_schema_template" "t2" {
-  name      = "t2"
-  schema_id = data.mso_schema.schema2.id
+data "aci_l4_l7_logical_interface" "l4_l7_fw_prov_prov_inf" {
+  l4_l7_device_dn = data.aci_l4_l7_device.l4_l7_fw
+  name            = "prov_inf"
+}
+data "aci_l4_l7_logical_interface" "l4_l7_fw_prov_cons_inf" {
+  l4_l7_device_dn = data.aci_l4_l7_device.l4_l7_fw
+  name            = "cons_inf"
 }
 
-data "mso_schema_template_bd" "bd1" {
-  schema_id     = data.mso_schema.schema1.id
-  template_name = "t1"
+data "aci_l4_l7_logical_interface" "l4_l7_adc_lb_prov_inf" {
+  l4_l7_device_dn = data.aci_l4_l7_device.l4_l7_adc_lb
+  name            = "prov_inf"
+}
+
+data "aci_l4_l7_logical_interface" "l4_l7_adc_lb_cons_inf" {
+  l4_l7_device_dn = data.aci_l4_l7_device.l4_l7_adc_lb
+  name            = "cons_inf"
+}
+
+data "aci_l4_l7_logical_interface" "l4_l7_others_prov_inf" {
+  l4_l7_device_dn = data.aci_l4_l7_device.l4_l7_others
+  name            = "prov_inf"
+}
+
+data "aci_l4_l7_logical_interface" "l4_l7_others_cons_inf" {
+  l4_l7_device_dn = data.aci_l4_l7_device.l4_l7_others
+  name            = "cons_inf"
+}
+
+# ND Config begins
+
+data "mso_site" "ansible_test" {
+  name = "ansible_test"
+}
+
+data "mso_tenant" "ansible_test" {
+  name = "ansible_test"
+}
+
+resource "mso_schema" "tf_schema_sg" {
+  name = "tf_schema_sg"
+  template {
+    name         = "template1"
+    display_name = "template1"
+    tenant_id    = data.mso_tenant.ansible_test.id
+  }
+}
+
+resource "mso_schema_template_vrf" "vrf1" {
+  schema_id    = mso_schema.tf_schema_sg.id
+  template     = one(mso_schema.tf_schema_sg.template).name
+  name         = "vrf1"
+  display_name = "vrf1"
+}
+
+resource "mso_schema_template_vrf" "vrf2" {
+  schema_id    = mso_schema.tf_schema_sg.id
+  template     = one(mso_schema.tf_schema_sg.template).name
+  name         = "vrf2"
+  display_name = "vrf2"
+}
+
+resource "mso_schema_template_bd" "bd1" {
+  schema_id     = mso_schema.tf_schema_sg.id
+  template_name = one(mso_schema.tf_schema_sg.template).name
   name          = "bd1"
+  display_name  = "bd1"
+  vrf_name      = mso_schema_template_vrf.vrf1.name
+  arp_flooding  = true
 }
 
-data "mso_schema_template_bd" "bd2" {
-  schema_id     = data.mso_schema.schema2.id
-  template_name = "t2"
+resource "mso_schema_template_bd" "bd2" {
+  schema_id     = mso_schema.tf_schema_sg.id
+  template_name = one(mso_schema.tf_schema_sg.template).name
   name          = "bd2"
+  display_name  = "bd2"
+  vrf_name      = mso_schema_template_vrf.vrf2.name
+  arp_flooding  = true
 }
 
-data "mso_schema_template_contract" "c1" {
-  schema_id     = data.mso_schema.schema1.id
-  template_name = "t1"
-  contract_name = "c1"
+resource "mso_schema_template_filter_entry" "filter_entry1" {
+  schema_id          = mso_schema.tf_schema_sg.id
+  template_name      = one(mso_schema.tf_schema_sg.template).name
+  name               = "filter1"
+  display_name       = "filter1"
+  entry_name         = "filter_entry1"
+  entry_display_name = "filter_entry1"
 }
 
-data "mso_schema_template_service_graph" "sg1" {
-  schema_id          = data.mso_schema.schema1.id
-  template_name      = "t1"
-  service_graph_name = "sg1"
-}
-
-resource "mso_schema_site" "schema_site_1" {
-  schema_id     = data.mso_schema.schema1.id
-  site_id       = data.mso_site.site1.id
-  template_name = data.mso_schema_template_service_graph.sg1.template_name
-}
-
-resource "mso_schema_site_contract_service_graph" "example" {
-  schema_id          = data.mso_schema.schema1.id
-  template_name      = "t1"
-  contract_name      = "c1"
-  service_graph_name = "sg1"
-  site_id            = data.mso_site.site1.id
-  node_relationship {
-    provider_connector_cluster_interface      = "example_provider_cluster_interface"
-    provider_connector_redirect_policy_tenant = "example_tenant"
-    provider_connector_redirect_policy        = "example_redirect_policy"
-    consumer_connector_cluster_interface      = "example_consumer_cluster_interface"
-    consumer_connector_redirect_policy_tenant = "example_tenant"
-    consumer_connector_redirect_policy        = "example_redirect_policy"
-    consumer_subnet_ips                       = ["1.1.1.1/24", "2.2.2.2/24"]
-  }
-  node_relationship {
-    provider_connector_cluster_interface = "example_provider_cluster_interface"
-    consumer_connector_cluster_interface = "example_consumer_cluster_interface"
+resource "mso_schema_template_contract" "template_c1" {
+  schema_id     = mso_schema.tf_schema_sg.id
+  template_name = one(mso_schema.tf_schema_sg.template).name
+  contract_name = "contract1"
+  filter_relationship {
+    filter_name = mso_schema_template_filter_entry.filter_entry1.name
+    filter_type = "bothWay"
   }
 }
 
-# Cloud Network Controller site configuration
-resource "mso_schema_site_contract_service_graph" "example" {
-  schema_id          = data.mso_schema.schema1.id
-  template_name      = "t1"
-  contract_name      = "c1"
-  service_graph_name = "sg1"
-  site_id            = data.mso_site.site1.id
+resource "mso_schema_template_vrf_contract" "template_vrf_contract" {
+  schema_id         = mso_schema.tf_schema_sg.id
+  template_name     = one(mso_schema.tf_schema_sg.template).name
+  vrf_name          = mso_schema_template_vrf.vrf1.name
+  relationship_type = "provider"
+  contract_name     = mso_schema_template_contract.template_c1.contract_name
+}
+
+resource "mso_schema_template_service_graph" "template_sg1" {
+  schema_id          = mso_schema.tf_schema_sg.id
+  template_name      = one(mso_schema.tf_schema_sg.template).name
+  service_graph_name = "service_graph1"
+  service_node {
+    type = "firewall"
+  }
+  service_node {
+    type = "load-balancer"
+  }
+  service_node {
+    type = "other"
+  }
+}
+
+resource "mso_schema_template_l3out" "template_l3out" {
+  schema_id     = mso_schema.tf_schema_sg.id
+  template_name = one(mso_schema.tf_schema_sg.template).name
+  l3out_name    = "l3out"
+  display_name  = "l3out"
+  vrf_name      = mso_schema_template_vrf.vrf1.name
+}
+
+resource "mso_schema_template_external_epg" "ext_epg" {
+  schema_id         = mso_schema.tf_schema_sg.id
+  template_name     = one(mso_schema.tf_schema_sg.template).name
+  external_epg_name = "ext_epg"
+  display_name      = "ext_epg"
+  vrf_name          = mso_schema_template_vrf.vrf1.name
+  l3out_name        = mso_schema_template_l3out.template_l3out.l3out_name
+}
+
+resource "mso_schema_template_external_epg_contract" "ext_epg_c1_cons" {
+  schema_id         = mso_schema.tf_schema_sg.id
+  template_name     = one(mso_schema.tf_schema_sg.template).name
+  external_epg_name = mso_schema_template_external_epg.ext_epg.external_epg_name
+  relationship_type = "consumer"
+  contract_name     = mso_schema_template_contract.template_c1.contract_name
+}
+
+resource "mso_schema_template_external_epg_contract" "ext_epg_c1_prov" {
+  schema_id         = mso_schema.tf_schema_sg.id
+  template_name     = one(mso_schema.tf_schema_sg.template).name
+  external_epg_name = mso_schema_template_external_epg.ext_epg.external_epg_name
+  relationship_type = "provider"
+  contract_name     = mso_schema_template_contract.template_c1.contract_name
+  depends_on = [mso_schema_template_external_epg_contract.ext_epg_c1_cons
+  ]
+}
+
+resource "mso_schema_template_contract_service_graph" "template_contract_sg" {
+  schema_id          = mso_schema.tf_schema_sg.id
+  template_name      = one(mso_schema.tf_schema_sg.template).name
+  contract_name      = mso_schema_template_contract.template_c1.contract_name
+  service_graph_name = mso_schema_template_service_graph.template_sg1.service_graph_name
+  node_relationship {
+    consumer_connector_bd_name = mso_schema_template_bd.bd1.name
+    provider_connector_bd_name = mso_schema_template_bd.bd2.name
+  }
+  node_relationship {
+    consumer_connector_bd_name = mso_schema_template_bd.bd1.name
+    provider_connector_bd_name = mso_schema_template_bd.bd2.name
+  }
+  node_relationship {
+    consumer_connector_bd_name = mso_schema_template_bd.bd1.name
+    provider_connector_bd_name = mso_schema_template_bd.bd2.name
+  }
+  depends_on = [mso_schema_template_external_epg_contract.ext_epg_c1_prov, mso_schema_template_external_epg_contract.ext_epg_c1_cons
+  ]
+}
+
+resource "mso_schema_site" "site1" {
+  schema_id     = mso_schema.tf_schema_sg.id
+  template_name = one(mso_schema.tf_schema_sg.template).name
+  site_id       = data.mso_site.ansible_test.id
+}
+
+# Note: This resource(mso_schema_site_service_graph) is supported only for NDO 4.1.1i and above.
+# Deletion of site Service Graph is not supported by the API. 
+# Site Service Graph will be removed when site is disassociated from the template or when Service Graph is removed at the template level.
+resource "mso_schema_site_service_graph" "site_service_graph" {
+  schema_id          = mso_schema.tf_schema_sg.id
+  template_name      = one(mso_schema.tf_schema_sg.template).name
+  site_id            = mso_schema_site.site1.site_id
+  service_graph_name = mso_schema_template_contract_service_graph.template_contract_sg.service_graph_name
+  service_node {
+    device_dn = data.aci_l4_l7_device.l4_l7_fw
+  }
+  service_node {
+    device_dn = data.aci_l4_l7_device.l4_l7_adc_lb
+  }
+  service_node {
+    device_dn = data.aci_l4_l7_device.l4_l7_others
+  }
+}
+
+resource "mso_schema_site_contract_service_graph" "site_contract_service_graph" {
+  schema_id          = mso_schema.tf_schema_sg.id
+  template_name      = one(mso_schema.tf_schema_sg.template).name
+  site_id            = mso_schema_site_service_graph.site_service_graph.site_id
+  contract_name      = mso_schema_template_contract.template_c1.contract_name
+  service_graph_name = mso_schema_template_contract_service_graph.template_contract_sg.service_graph_name
+  node_relationship {
+    provider_connector_cluster_interface = data.aci_l4_l7_logical_interface.l4_l7_fw_prov_prov_inf.name
+    consumer_connector_cluster_interface = data.aci_l4_l7_logical_interface.l4_l7_fw_prov_cons_inf.name
+  }
+  node_relationship {
+    provider_connector_cluster_interface = data.aci_l4_l7_logical_interface.l4_l7_adc_lb_prov_inf.name
+    consumer_connector_cluster_interface = data.aci_l4_l7_logical_interface.l4_l7_adc_lb_cons_inf.name
+    consumer_subnet_ips = [
+      "1.1.1.1/24",
+      "2.2.2.2/24"
+    ]
+  }
+  node_relationship {
+    provider_connector_cluster_interface = data.aci_l4_l7_logical_interface.l4_l7_others_prov_inf.name
+    consumer_connector_cluster_interface = data.aci_l4_l7_logical_interface.l4_l7_others_cons_inf.name
+  }
 }
