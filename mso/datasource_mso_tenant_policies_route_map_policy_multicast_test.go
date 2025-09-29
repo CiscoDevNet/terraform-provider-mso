7368da25f2eb955f63f484d57546f08137e8b730
Shreyas <ssrish@cisco.com>
2025-09-29 10:38:39 -0400

    [ignore] Modified mso_tenant_policies_route_map_policy_multicast data source test file to check multiple route map policies

--- mso/datasource_mso_tenant_policies_route_map_policy_multicast_test.go
+++ mso/datasource_mso_tenant_policies_route_map_policy_multicast_test.go
@@ -16,16 +16,25 @@ func TestAccMSOTenantPoliciesMcastRouteMapPolicyDataSource(t *testing.T) {
 				PreConfig: func() { fmt.Println("Test: Multicast Route Map Policy Data Source") },
 				Config:    testAccMSOTenantPoliciesMcastRouteMapPolicyDataSource(),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("data.mso_tenant_policies_route_map_policy_multicast.route_map_policy_multicast", "name", "tf_test_route_map_policy_multicast"),
-					resource.TestCheckResourceAttr("data.mso_tenant_policies_route_map_policy_multicast.route_map_policy_multicast", "description", "Terraform test Route Map Policy for Multicast"),
-					resource.TestCheckResourceAttr("data.mso_tenant_policies_route_map_policy_multicast.route_map_policy_multicast", "route_map_multicast_entries.#", "1"),
-					customTestCheckResourceTypeSetAttr("data.mso_tenant_policies_route_map_policy_multicast.route_map_policy_multicast", "route_map_multicast_entries",
+					resource.TestCheckResourceAttr("mso_tenant_policies_route_map_policy_multicast.route_map_policy_multicast", "name", "tf_test_route_map_policy_multicast"),
+					resource.TestCheckResourceAttr("mso_tenant_policies_route_map_policy_multicast.route_map_policy_multicast", "description", "Terraform test Route Map Policy for Multicast adding extra entry"),
+					resource.TestCheckResourceAttr("mso_tenant_policies_route_map_policy_multicast.route_map_policy_multicast", "route_map_multicast_entries.#", "2"),
+					customTestCheckResourceTypeSetAttr("mso_tenant_policies_route_map_policy_multicast.route_map_policy_multicast", "route_map_multicast_entries",
 						map[string]string{
 							"order":               "1",
 							"group_ip":            "226.2.2.2/8",
 							"source_ip":           "1.1.1.1/1",
 							"rendezvous_point_ip": "1.1.1.2",
 							"action":              "permit",
+						},
+					),
+					customTestCheckResourceTypeSetAttr("mso_tenant_policies_route_map_policy_multicast.route_map_policy_multicast", "route_map_multicast_entries",
+						map[string]string{
+							"order":               "2",
+							"group_ip":            "226.3.3.3/24",
+							"source_ip":           "2.2.2.2/2",
+							"rendezvous_point_ip": "2.2.2.3",
+							"action":              "deny",
 						},
 					),
 				),
@@ -39,5 +48,5 @@ func testAccMSOTenantPoliciesMcastRouteMapPolicyDataSource() string {
 	data "mso_tenant_policies_route_map_policy_multicast" "route_map_policy_multicast" {
 	    template_id        = mso_tenant_policies_route_map_policy_multicast.route_map_policy_multicast.template_id
 	    name               = "tf_test_route_map_policy_multicast"
-    }`, testAccMSOTenantPoliciesMcastRouteMapPolicyConfigCreate())
+    }`, testAccMSOTenantPoliciesMcastRouteMapPolicyConfigUpdateAddingExtraEntry())
 }
