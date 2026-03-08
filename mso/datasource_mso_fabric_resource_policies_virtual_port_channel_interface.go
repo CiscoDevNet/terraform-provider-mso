78d9c816c111c94cf9993faa979825211885828f
samitab <samitab@cisco.com>
2026-03-08 23:21:51 +1000

    [minor_change] Add mso_fabric_resource_policies_virtual_port_channel_interface resource and datasource.

---
+++ mso/datasource_mso_fabric_resource_policies_virtual_port_channel_interface.go
@@ -0,0 +1,75 @@
+package mso
+
+import (
+	"log"
+
+	"github.com/ciscoecosystem/mso-go-client/client"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func dataSourceMSOVirtualPortChannelInterface() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceMSOFabricResourcePoliciesVirtualPortChannelInterfaceRead,
+
+		SchemaVersion: version,
+
+		Schema: map[string]*schema.Schema{
+			"template_id": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
+				Description:  "Fabric Resource template ID.",
+			},
+			"name": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
+				Description:  "Virtual Port Channel Interface name.",
+			},
+			"uuid":        {Type: schema.TypeString, Computed: true},
+			"description": {Type: schema.TypeString, Computed: true},
+
+			"node_1": {Type: schema.TypeString, Computed: true},
+			"node_2": {Type: schema.TypeString, Computed: true},
+
+			"node_1_interfaces": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"node_2_interfaces": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+
+			"interface_policy_group_uuid": {Type: schema.TypeString, Computed: true},
+
+			"interface_descriptions": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"node":        {Type: schema.TypeString, Computed: true},
+						"interface":   {Type: schema.TypeString, Computed: true},
+						"description": {Type: schema.TypeString, Computed: true},
+					},
+				},
+			},
+		},
+	}
+}
+
+func dataSourceMSOFabricResourcePoliciesVirtualPortChannelInterfaceRead(d *schema.ResourceData, m any) error {
+	log.Printf("[DEBUG] MSO VPC Interface Data Source - Beginning Read: %v", d.Id())
+	msoClient := m.(*client.Client)
+
+	templateId := d.Get("template_id").(string)
+	policyName := d.Get("name").(string)
+
+	setVPCInterfaceData(d, msoClient, templateId, policyName)
+
+	log.Printf("[DEBUG] MSO VPC Interface Data Source - Read Complete: %v", d.Id())
+	return nil
+}
---
+++ mso/resource_mso_fabric_resource_policies_virtual_port_channel_interface_test.go
@@ -0,0 +1,203 @@
+package mso
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccMSOVirtualPortChannelInterfaceResource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				PreConfig: func() {
+					fmt.Println("Test: Virtual Port Channel Interface Resource - Create")
+				},
+				Config: testAccMSOVirtualPortChannelInterfaceConfigCreate(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if",
+						"name",
+						"tf_test_vpc_if",
+					),
+					resource.TestCheckResourceAttr(
+						"mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if",
+						"description",
+						"Terraform test VPC Interface",
+					),
+					resource.TestCheckResourceAttr(
+						"mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if",
+						"node_1",
+						"101",
+					),
+					resource.TestCheckResourceAttr(
+						"mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if",
+						"node_2",
+						"102",
+					),
+					resource.TestCheckResourceAttr(
+						"mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if",
+						"node_1_interfaces.#",
+						"2",
+					),
+					resource.TestCheckResourceAttr(
+						"mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if",
+						"node_2_interfaces.#",
+						"1",
+					),
+					resource.TestCheckResourceAttr(
+						"mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if",
+						"interface_descriptions.#",
+						"1",
+					),
+					resource.TestCheckResourceAttrSet(
+						"mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if",
+						"uuid",
+					),
+				),
+			},
+			{
+				PreConfig: func() {
+					fmt.Println("Test: Virtual Port Channel Interface Resource - Update")
+				},
+				Config: testAccMSOVirtualPortChannelInterfaceConfigUpdate(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if",
+						"description",
+						"Terraform test VPC Interface (updated)",
+					),
+					resource.TestCheckResourceAttr(
+						"mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if",
+						"node_1_interfaces.#",
+						"3",
+					),
+					resource.TestCheckResourceAttr(
+						"mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if",
+						"node_2_interfaces.#",
+						"3",
+					),
+					resource.TestCheckResourceAttr(
+						"mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if",
+						"interface_descriptions.#",
+						"2",
+					),
+					resource.TestCheckResourceAttrSet(
+						"mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if",
+						"uuid",
+					),
+				),
+			},
+			{
+				PreConfig: func() {
+					fmt.Println("Test: Virtual Port Channel Interface Resource - Removed Descriptions")
+				},
+				Config: testAccMSOVirtualPortChannelInterfaceConfigRemoveDescriptions(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if",
+						"description",
+						"Terraform test VPC Interface (removed descriptions)",
+					),
+					resource.TestCheckResourceAttr(
+						"mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if",
+						"node_1_interfaces.#",
+						"3",
+					),
+					resource.TestCheckResourceAttr(
+						"mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if",
+						"node_2_interfaces.#",
+						"3",
+					),
+					resource.TestCheckResourceAttr(
+						"mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if",
+						"interface_descriptions.#",
+						"0",
+					),
+					resource.TestCheckResourceAttrSet(
+						"mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if",
+						"uuid",
+					),
+				),
+			},
+			{
+				ResourceName:      "mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+		CheckDestroy: testCheckResourceDestroyPolicyWithPathAttributesAndArguments("mso_fabric_resource_policies_virtual_port_channel_interface", "fabricResourceTemplate", "template", "virtualPortChannels"),
+	})
+}
+
+func testAccMSOVirtualPortChannelInterfaceConfigCreate() string {
+	return fmt.Sprintf(`%s
+resource "mso_fabric_resource_policies_virtual_port_channel_interface" "vpc_if" {
+  template_id = mso_template.template_fabric_resource.id
+  name        = "tf_test_vpc_if"
+  description = "Terraform test VPC Interface"
+  node_1 = "101"
+  node_2 = "102"
+  node_1_interfaces = ["1/1", "1/10-11"]
+  node_2_interfaces = ["1/2"]
+  interface_policy_group_uuid = "c0e13c44-a1ee-4fe7-8630-c02e9b2520aa"
+  // interface_policy_group_uuid will be validated/required once the
+  // interface policy group resource merges (PR #449).
+  
+  interface_descriptions {
+      node         = "101"
+      interface    = "1/1"
+      description  = "Terraform test interface description"
+  }
+}
+`, testAccMSOTemplateResourceFabricResourceConfig())
+}
+
+func testAccMSOVirtualPortChannelInterfaceConfigUpdate() string {
+	return fmt.Sprintf(`%s
+resource "mso_fabric_resource_policies_virtual_port_channel_interface" "vpc_if" {
+  template_id = mso_template.template_fabric_resource.id
+  name        = "tf_test_vpc_if"
+  description = "Terraform test VPC Interface (updated)"
+  node_1 = "101"
+  node_2 = "102"
+  node_1_interfaces = ["1/1", "1/2-5", "1/7"]
+  node_2_interfaces = ["1/2", "1/3", "1/5-7"]
+  interface_policy_group_uuid = "c0e13c44-a1ee-4fe7-8630-c02e9b2520aa"
+  // interface_policy_group_uuid will be validated/required once the
+  // interface policy group resource merges (PR #449).
+
+  interface_descriptions {
+      node         = "101"
+      interface    = "1/1"
+      description  = "Terraform test interface description 101"
+  }
+
+  interface_descriptions {
+      node         = "102"
+      interface    = "1/3"
+      description  = "Terraform test interface description 102"
+  }
+}
+`, testAccMSOTemplateResourceFabricResourceConfig())
+}
+
+func testAccMSOVirtualPortChannelInterfaceConfigRemoveDescriptions() string {
+	return fmt.Sprintf(`%s
+resource "mso_fabric_resource_policies_virtual_port_channel_interface" "vpc_if" {
+  template_id = mso_template.template_fabric_resource.id
+  name        = "tf_test_vpc_if"
+  description = "Terraform test VPC Interface (removed descriptions)"
+  node_1 = "101"
+  node_2 = "102"
+  node_1_interfaces = ["1/1", "1/2-5", "1/7"]
+  node_2_interfaces = ["1/2", "1/3", "1/5-7"]
+  interface_policy_group_uuid = "c0e13c44-a1ee-4fe7-8630-c02e9b2520aa"
+  // interface_policy_group_uuid will be validated/required once the
+  // interface policy group resource lands (PR #449).
+}
+`, testAccMSOTemplateResourceFabricResourceConfig())
+}
