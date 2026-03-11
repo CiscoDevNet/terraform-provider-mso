f94327eae729231e929118b42deefd4c75b97e17
samitab <samitab@cisco.com>
2026-03-11 18:00:07 +1000

    [ignore] Update tests

--- mso/datasource_mso_fabric_resource_policies_virtual_port_channel_interface_test.go
+++ mso/datasource_mso_fabric_resource_policies_virtual_port_channel_interface_test.go
@@ -16,44 +16,25 @@ func TestAccMSOVirtualPortChannelInterfaceDataSource(t *testing.T) {
 				PreConfig: func() { fmt.Println("Test: VPC Interface Data Source") },
 				Config:    testAccMSOVirtualPortChannelInterfaceDataSource(),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr(
-						"mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if",
-						"name",
-						"tf_test_vpc_if",
-					),
-					resource.TestCheckResourceAttr(
-						"mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if",
-						"description",
-						"Terraform test VPC Interface",
-					),
-					resource.TestCheckResourceAttr(
-						"mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if",
-						"node_1",
-						"101",
-					),
-					resource.TestCheckResourceAttr(
-						"mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if",
-						"node_2",
-						"102",
-					),
-					resource.TestCheckResourceAttr(
-						"mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if",
-						"node_1_interfaces.#",
-						"2",
-					),
-					resource.TestCheckResourceAttr(
-						"mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if",
-						"node_2_interfaces.#",
-						"1",
-					),
-					resource.TestCheckResourceAttr(
-						"mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if",
-						"interface_descriptions.#",
-						"1",
-					),
-					resource.TestCheckResourceAttrSet(
-						"mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if",
-						"uuid",
+					resource.TestCheckResourceAttr("data.mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if", "name", "tf_test_vpc_if"),
+					resource.TestCheckResourceAttr("data.mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if", "description", "Terraform test VPC Interface"),
+					resource.TestCheckResourceAttr("data.mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if", "node_1", "101"),
+					resource.TestCheckResourceAttr("data.mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if", "node_2", "102"),
+					resource.TestCheckResourceAttr("data.mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if", "node_1_interfaces.#", "2"),
+					resource.TestCheckResourceAttr("data.mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if", "node_1_interfaces.0", "1/1"),
+					resource.TestCheckResourceAttr("data.mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if", "node_1_interfaces.1", "1/10-11"),
+					resource.TestCheckResourceAttr("data.mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if", "node_2_interfaces.#", "1"),
+					resource.TestCheckResourceAttr("data.mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if", "node_2_interfaces.0", "1/2"),
+					resource.TestCheckResourceAttr("data.mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if", "interface_descriptions.#", "1"),
+					resource.TestCheckResourceAttrSet("data.mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if", "uuid"),
+					customTestCheckResourceTypeSetAttr(
+						"data.mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if",
+						"interface_descriptions",
+						map[string]string{
+							"node":        "101",
+							"interface":   "1/1",
+							"description": "Terraform test interface description",
+						},
 					),
 				),
 			},
@@ -64,7 +45,8 @@ func TestAccMSOVirtualPortChannelInterfaceDataSource(t *testing.T) {
 func testAccMSOVirtualPortChannelInterfaceDataSource() string {
 	return fmt.Sprintf(`%s
 	data "mso_fabric_resource_policies_virtual_port_channel_interface" "vpc_if" {
-	    template_id        = mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if.template_id
-	    name               = "tf_test_vpc_if"
-    }`, testAccMSOVirtualPortChannelInterfaceConfigCreate())
+		template_id = mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if.template_id
+		name        = "tf_test_vpc_if"
+	}
+	`, testAccMSOVirtualPortChannelInterfaceConfigCreate())
 }
