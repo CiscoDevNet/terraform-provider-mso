6e5dcd5da9c5b2cc2b47451665be97f656efaa5e
samitab <samitab@cisco.com>
2026-03-18 06:31:31 +1000

    [ignore] Add test with interface_policy_group_uuid resource

--- mso/resource_mso_fabric_resource_policies_virtual_port_channel_interface_test.go
+++ mso/resource_mso_fabric_resource_policies_virtual_port_channel_interface_test.go
@@ -29,6 +29,7 @@ func TestAccMSOVirtualPortChannelInterfaceResource(t *testing.T) {
 					resource.TestCheckResourceAttr("mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if", "node_2_interfaces.0", "1/2"),
 					resource.TestCheckResourceAttr("mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if", "interface_descriptions.#", "1"),
 					resource.TestCheckResourceAttrSet("mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if", "uuid"),
+					resource.TestCheckResourceAttrSet("mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if", "interface_policy_group_uuid"),
 					customTestCheckResourceTypeSetAttr(
 						"mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if",
 						"interface_descriptions",
@@ -60,6 +61,7 @@ func TestAccMSOVirtualPortChannelInterfaceResource(t *testing.T) {
 					resource.TestCheckResourceAttr("mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if", "node_2_interfaces.2", "1/5-7"),
 					resource.TestCheckResourceAttr("mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if", "interface_descriptions.#", "2"),
 					resource.TestCheckResourceAttrSet("mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if", "uuid"),
+					resource.TestCheckResourceAttrSet("mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if", "interface_policy_group_uuid"),
 					customTestCheckResourceTypeSetAttr(
 						"mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if",
 						"interface_descriptions",
@@ -91,6 +93,7 @@ func TestAccMSOVirtualPortChannelInterfaceResource(t *testing.T) {
 					resource.TestCheckResourceAttr("mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if", "node_2_interfaces.#", "3"),
 					resource.TestCheckResourceAttr("mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if", "interface_descriptions.#", "0"),
 					resource.TestCheckResourceAttrSet("mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if", "uuid"),
+					resource.TestCheckResourceAttrSet("mso_fabric_resource_policies_virtual_port_channel_interface.vpc_if", "interface_policy_group_uuid"),
 				),
 			},
 			{
@@ -109,20 +112,16 @@ func TestAccMSOVirtualPortChannelInterfaceResource(t *testing.T) {
 }
 
 func testAccMSOVirtualPortChannelInterfaceConfigCreate() string {
-	return fmt.Sprintf(`%s
+	return fmt.Sprintf(`%s%s
 	resource "mso_fabric_resource_policies_virtual_port_channel_interface" "vpc_if" {
 		template_id = mso_template.template_fabric_resource.id
+		interface_policy_group_uuid = mso_fabric_policies_interface_setting.%s_portchannel.uuid
 		name        = "tf_test_vpc_if"
 		description = "Terraform test VPC Interface"
 		node_1      = "101"
 		node_2      = "102"
-
 		node_1_interfaces = ["1/1", "1/10-11"]
 		node_2_interfaces = ["1/2"]
-
-		interface_policy_group_uuid = "c0e13c44-a1ee-4fe7-8630-c02e9b2520aa"
-		// interface_policy_group_uuid will be validated/required once the
-		// interface policy group resource merges (PR #449).
 
 		interface_descriptions {
 			node        = "101"
@@ -130,24 +129,20 @@ func testAccMSOVirtualPortChannelInterfaceConfigCreate() string {
 			description = "Terraform test interface description"
 		}
 	}
-	`, testAccMSOTemplateResourceFabricResourceConfig())
+	`, testAccMSOTemplateResourceFabricResourceConfig(), testAccMSOFabricPoliciesInterfaceSettingPortChannelConfigCreate(), msoFabricPolicyTemplateInterfaceSettingName)
 }
 
 func testAccMSOVirtualPortChannelInterfaceConfigUpdate() string {
-	return fmt.Sprintf(`%s
+	return fmt.Sprintf(`%s%s
 	resource "mso_fabric_resource_policies_virtual_port_channel_interface" "vpc_if" {
 		template_id = mso_template.template_fabric_resource.id
+		interface_policy_group_uuid = mso_fabric_policies_interface_setting.%s_portchannel.uuid
 		name        = "tf_test_vpc_if"
 		description = "Terraform test VPC Interface (updated)"
 		node_1      = "103"
 		node_2      = "104"
-
 		node_1_interfaces = ["1/1", "1/2-5", "1/7"]
 		node_2_interfaces = ["1/2", "1/3", "1/5-7"]
-
-		interface_policy_group_uuid = "c0e13c44-a1ee-4fe7-8630-c02e9b2520aa"
-		// interface_policy_group_uuid will be validated/required once the
-		// interface policy group resource merges (PR #449).
 
 		interface_descriptions {
 			node        = "103"
@@ -161,24 +156,20 @@ func testAccMSOVirtualPortChannelInterfaceConfigUpdate() string {
 			// No description on purpose
 		}
 	}
-	`, testAccMSOTemplateResourceFabricResourceConfig())
+	`, testAccMSOTemplateResourceFabricResourceConfig(), testAccMSOFabricPoliciesInterfaceSettingPortChannelConfigCreate(), msoFabricPolicyTemplateInterfaceSettingName)
 }
 
 func testAccMSOVirtualPortChannelInterfaceConfigRemoveDescriptions() string {
-	return fmt.Sprintf(`%s
+	return fmt.Sprintf(`%s%s
 	resource "mso_fabric_resource_policies_virtual_port_channel_interface" "vpc_if" {
 		template_id = mso_template.template_fabric_resource.id
+		interface_policy_group_uuid = mso_fabric_policies_interface_setting.%s_portchannel.uuid
 		name        = "tf_test_vpc_if"
 		description = "Terraform test VPC Interface (removed descriptions)"
 		node_1      = "103"
 		node_2      = "104"
-
 		node_1_interfaces = ["1/1", "1/2-5", "1/7"]
 		node_2_interfaces = ["1/2", "1/3", "1/5-7"]
-
-		interface_policy_group_uuid = "c0e13c44-a1ee-4fe7-8630-c02e9b2520aa"
-		// interface_policy_group_uuid will be validated/required once the
-		// interface policy group resource lands (PR #449).
 	}
-	`, testAccMSOTemplateResourceFabricResourceConfig())
+	`, testAccMSOTemplateResourceFabricResourceConfig(), testAccMSOFabricPoliciesInterfaceSettingPortChannelConfigCreate(), msoFabricPolicyTemplateInterfaceSettingName)
 }
