2bd60fdca9fd9ec4a1c8406a7d1592584c6b4f34
akinross <akinross@cisco.com>
2026-07-02 10:51:23 +0200

    [minor_change] Deprecate mso_schema_template_deploy resource in favor of mso_schema_template_deploy_ndo for ND-based NDO

--- mso/resource_mso_schema_template_deploy.go
+++ mso/resource_mso_schema_template_deploy.go
@@ -53,6 +53,7 @@ func resourceMSOSchemaTemplateDeploy() *schema.Resource {
 				ValidateFunc: validation.StringLenBetween(1, 1000),
 			},
 		}),
+		DeprecationMessage: "mso_schema_template_deploy is deprecated: use mso_schema_template_deploy_ndo instead for ND-based NDO.",
 	}
 }
 
